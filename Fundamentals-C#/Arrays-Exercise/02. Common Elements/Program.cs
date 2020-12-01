486e2491fd76d34cf72679a190b9a27b7cb64dde
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-01 20:12:41 +0200

    Added Arrays Exercise

---
+++ Fundamentals-C#/Arrays-Exercise/02. Common Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Common_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] arr2 = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                for (int j = 0; j < arr1.Length; j++)
+                {
+                    if (arr1[j] == arr2[i])
+                    {
+                        Console.Write($"{arr2[i]} ");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Fundamentals-C#/Arrays-Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _05._Top_Integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                                .Split(" ")
+                                .Select(int.Parse)
+                                .ToArray();
+            bool isBigger = true;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int currentNum = arr[i];
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] >= currentNum)
+                    {
+                        isBigger = false;
+                        break;
+                    }
+                }
+                if (isBigger)
+                {
+                    Console.Write(currentNum + " ");
+                }
+                isBigger = true;
+            }
+        }
+    }
+}

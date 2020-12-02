727f90702d6498eb56aa74e319d00a83ade476d7
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-02 23:00:00 +0200

    Added Objects and Classes - Exercise

---
+++ Fundamentals-C#/Objects and Classes - Exercise/06. Vehicle Catalogue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace _06._Vehicle_Catalogue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Vehicle
+    {
+        public Vehicle(string type, string model, string colore, string horsePower)
+        {
+            Type = type;
+            Model = model;
+            Colore = colore;
+            HorsePower = horsePower;
+        }
+
+        public string Type { get; set; }
+
+        public string Model { get; set; }
+
+        public string Colore { get; set; }
+
+        public string HorsePower { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Type: {(Type == "car" ? "car" : "Truck")}");
+            return
+        }
+    }
+
+}

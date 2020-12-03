dae9ef8f3c083b3030f7b4a19731881c56fccc06
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-03 07:59:51 +0200

    Added Associative Arrays - Lab

---
+++ Fundamentals-C#/Associative-Arrays-Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            
+            var counts = new SortedDictionary<double, int>();
+
+            foreach (var num in nums)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts.Add(num, 1);
+                }
+            }
+
+            foreach (var num in counts)
+            {
+                Console.WriteLine($"{num.Key} -> {num.Value}");
+            }
+        }
+    }
+}

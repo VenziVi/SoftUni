2e3ad41ed9cb24b3fb5e18ad58e608792c7db1d9
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-09 07:57:18 +0200

    Added Data-Types-and-Variables-Lab

---
+++ Programming-Fundamentals-with-C#/Data-Types-and-Variables-Lab/01. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01._Convert_Meters_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+            decimal km = meters * 0.001M;
+            Console.WriteLine($"{km:F2}");
+        }
+    }
+}

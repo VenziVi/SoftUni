46a4fd9236fdd928351e11f022f0d48e7d812939
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-10 07:57:48 +0200

    Added Lists-Lab

---
+++ Programming-Fundamentals-with-C#/Lists-Lab/03. Merging Lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Merging_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list1 = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> list2 = Console.ReadLine()
+               .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < Math.Min(list1.Count, list2.Count); i++)
+            {
+                result.Add(list1[i]);
+                result.Add(list2[i]);
+            }
+
+            for (int i = Math.Min(list1.Count, list2.Count); i < Math.Max(list1.Count, list2.Count); i++)
+            {
+                if (i >= list1.Count)
+                {
+                    result.Add(list2[i]);
+                }
+                else
+                {
+                    result.Add(list1[i]);
+                }
+            }
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
---
+++ Programming-Fundamentals-with-C#/Lists-Lab/06. List Manipulation Basics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._List_Manipulation_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine().ToLower();
+
+            while (command != "end")
+            {
+                string[] function = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                switch (function[0].ToLower())
+                {
+                    case "add":
+                        numbers.Add(int.Parse(function[1]));
+                        break;
+                    case "remove":
+                        numbers.Remove(int.Parse(function[1]));
+                        break;
+                    case "removeat":
+                        numbers.RemoveAt(int.Parse(function[1]));
+                        break;
+                    case "insert":
+                        numbers.Insert(int.Parse(function[2]), int.Parse(function[1]));
+                        break;
+                    default:
+                        break;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(' ', numbers));
+
+        }
+    }
+}

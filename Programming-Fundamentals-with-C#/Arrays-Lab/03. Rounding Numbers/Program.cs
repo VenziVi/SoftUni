096d8c75d416b46c170f5bb7400f84e02f9b41f7
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-12 08:47:43 +0200

    Added Arrays-Lab

---
+++ Programming-Fundamentals-with-C#/Arrays-Lab/03. Rounding Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Rounding_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine()
+                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(double.Parse)
+                 .ToArray();
+            int[] splitNum = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                splitNum[i] = (int)Math.Round(arr[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{arr[i]} => {splitNum[i]}");
+            }
+        
+        }
+    }
+}

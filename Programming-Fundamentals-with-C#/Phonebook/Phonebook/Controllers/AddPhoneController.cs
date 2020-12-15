481637e68dca06b961ed916e3095acdcd98e5843
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-15 23:05:48 +0200

    Added AddPhoneController

---
+++ Programming-Fundamentals-with-C#/Phonebook/Phonebook/Controllers/AddPhoneController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Phonebook.Models;
+
+namespace Phonebook.Controllers
+{
+    public class AddPhoneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DataContext.Contacts);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                DataContext.Contacts.Add(model);
+            }
+            else
+            {
+                TempData["Error"] = "Невалидни данни!";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}

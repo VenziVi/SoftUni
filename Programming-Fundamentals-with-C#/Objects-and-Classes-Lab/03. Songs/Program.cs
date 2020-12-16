2a46e881a24bc7571da1eb89a92e1943effde816
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-16 19:45:03 +0200

    Added Objects-and-Classes-Lab

---
+++ Programming-Fundamentals-with-C#/Objects-and-Classes-Lab/03. Songs/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04._Songs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Song> songs = new List<Song>(n);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] data = Console.ReadLine()
+                    .Split('_', StringSplitOptions.RemoveEmptyEntries);
+
+                string type = data[0];
+                string name = data[1];
+                string time = data[2];
+
+                Song song = new Song();
+
+                song.TypeList = type;
+                song.Name = name;
+                song.Time = time;
+
+                songs.Add(song);
+            }
+
+            string typelist = Console.ReadLine();
+
+            if (typelist == "all")
+            {
+                foreach (var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                foreach (var song in songs)
+                {
+                    if (song.TypeList == typelist)
+                    {
+                        Console.WriteLine(song.Name);
+                    }
+                }
+            }
+        }
+    }
+
+    class Song
+    {
+        public string TypeList { get; set; }
+
+        public string Name { get; set; }
+
+        public string Time { get; set; }
+
+    }
+}
---
+++ Programming-Fundamentals-with-C#/Objects-and-Classes-Lab/06. Store Boxes/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _07._Store_Boxes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Box> boxes = new List<Box>();
+
+            string line = Console.ReadLine();
+
+            while (line != "end")
+            {
+                string[] cmdArgs = line.Split();
+                decimal priceForBox = int.Parse(cmdArgs[2]) * decimal.Parse(cmdArgs[3]);
+
+                
+                Box box = new Box(cmdArgs[0], new Item(cmdArgs[1], decimal.Parse(cmdArgs[3])), int.Parse(cmdArgs[2]), priceForBox);
+                box.Item = new Item(cmdArgs[1], decimal.Parse(cmdArgs[3]));
+                boxes.Add(box);
+
+                line = Console.ReadLine();
+            }
+            boxes = boxes.OrderByDescending(x => x.PriceForBox).ToList();
+            foreach (var box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+    }
+
+
+    class Box
+    {
+        public Box(string serialNumber, Item item, int itemQuantity, decimal priceForBox)
+        {
+            SerialNumber = serialNumber;
+            Item = new Item();
+            ItemQuantity = itemQuantity;
+            PriceForBox = priceForBox;
+        }
+
+        public string SerialNumber { get; set; }
+
+        public Item Item { get; set; }
+
+        public int ItemQuantity { get; set; }
+
+        public decimal PriceForBox { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(SerialNumber);
+            sb.AppendLine($"-- {Item.Name} - ${Item.Price:f2}: {ItemQuantity}");
+            sb.AppendLine($"-- ${PriceForBox:f2}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+    class Item
+    {
+        public Item()
+        {
+        }
+
+        public Item(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}

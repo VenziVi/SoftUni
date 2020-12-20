255d6a7298b193e55044cc4ee45885c14e634a85
Ventsislav Lazarov <vlazarov86@gmail.com>
2020-12-20 20:45:04 +0200

    added new function

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -6,13 +6,63 @@ namespace SimpleCalculator
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            //2 3 1 4 - * * 5 *
-            string[] expression = Console.ReadLine().Split(' ')
+            // 3 + 4  * 2 / ( 1 - 5 )
+
+            string[] expression = Console.ReadLine().Split(' ');
+
+            Stack<string> operatorStack = new Stack<string>();
+            string output = string.Empty;
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (IsOperator(expression[i]))
+                {
+                    if (operatorStack.Count > 0)
+                    {
+                        var oldElementArity = OperationPrecedence
+                            (operatorStack.Peek());
+                        var elementArity = OperationPrecedence
+                            (expression[i]);
+
+                        if (oldElementArity >= elementArity)
+                        {
+                            output += operatorStack.Pop() + " ";
+                        }
+                    }
+                    operatorStack.Push(expression[i]);
+
+                }
+                else if (expression[i] == "(")
+                {
+                    operatorStack.Push(expression[i]);
+                }
+                else if (expression[i] == ")")
+                {
+                    while (operatorStack.Peek() != "(")
+                    {
+                        output += operatorStack.Pop() + " ";
+                    }
+
+                    operatorStack.Pop();
+                }
+                else
+                {
+                    output += expression[i] + " ";
+                }
+            }
+
+            while (operatorStack.Count > 0)
+            {
+                output += operatorStack.Pop() + " ";
+            }
+
+            string[] outputExpression = output.Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Reverse().ToArray();
-            Stack<string> stack = new Stack<string>(expression);
-            PrintStack(stack);
+            Stack<string> stack = new Stack<string>(outputExpression);
+            
 
             while (stack.Count > 1)
             {
@@ -58,6 +108,27 @@ namespace SimpleCalculator
 
             throw new NotImplementedException();
         }
+
+        static int OperationPrecedence(string input)
+        {
+            switch (input)
+            {
+                case "+":
+                    return 2;
+                case "-":
+                    return 2;
+                case "/":
+                    return 3;
+                case "*":
+                    return 3;
+                case "(":
+                    return 1;
+                default:
+                    break;
+            }
+            throw new ArgumentException();
+        }
+
 
         static bool IsOperator(string input) 
         {

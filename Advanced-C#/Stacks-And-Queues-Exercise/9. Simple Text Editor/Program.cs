3fe8861f7fef9f3088d3b46efa52df7f1af8717b
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-01-17 20:12:28 +0200

    added new problem

---
+++ Advanced-C#/Stacks-And-Queues-Exercise/9. Simple Text Editor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string text = string.Empty;
+            Stack<string> undo = new Stack<string>();
+            undo.Push(text);
+
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                string[] input = command.Split();
+                string cmd = input[0];
+
+                if (cmd == "1")
+                {
+                    string newText = input[1];
+                    text = text.Insert(text.Length, newText);
+                    undo.Push(text);
+                }
+                else if (cmd == "2")
+                {
+                    int countToErase = int.Parse(input[1]);
+                    text = text.Remove(text.Length - countToErase, countToErase);
+                    undo.Push(text);
+                }
+                else if (cmd == "3")
+                {
+                    int index = int.Parse(input[1]) - 1;
+                    if (text.Length >= index)
+                    {
+                        string letter = text.Substring(index, 1);
+                        Console.WriteLine(letter);
+                    }
+                }
+                else if (cmd == "4")
+                {
+                    undo.Pop();
+                    text = undo.Peek();
+                }
+            }
+        }
+    }
+}

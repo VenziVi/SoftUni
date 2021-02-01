0b495f2f64bcedaaa7f289d85363dba10769fa90
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-02-01 23:21:00 +0200

    added new problem

---
+++ Advanced-C#/Streams-Files-and-Directories-Lab/2. Line Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace _2._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var reader = new StreamReader("../../../input.txt");
+            using (reader)
+            {
+                int num = 1;
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter("../../../output.txt"))
+                {
+                    while (line != null)
+                    {
+                        writer.WriteLine($"{num}. {line}");
+                        num++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            
+        }
+    }
+}

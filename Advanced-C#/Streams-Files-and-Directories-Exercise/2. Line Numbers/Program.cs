905b5ee715f79083dc322d71aa6eb7baa63e24d4
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-02-05 23:15:34 +0200

    added new problem

---
+++ Advanced-C#/Streams-Files-and-Directories-Exercise/2. Line Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _2._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../text.txt");
+            using (StreamWriter writer = new StreamWriter("../../../output.txt"))
+            {
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    int lettersCount = 0;
+                    int punctuationCount = 0;
+                    string line = lines[i];
+                    char[] letters = line.ToCharArray();
+
+                    for (int j = 0; j < letters.Length; j++)
+                    {
+                        if (char.IsLetter(letters[j]))
+                        {
+                            lettersCount++;
+                        }
+                        else if (char.IsPunctuation(letters[j]))
+                        {
+                            punctuationCount++;
+                        }
+                    }
+                    string output = ($"Line {i + 1}: {line} ({lettersCount})({punctuationCount})").ToString();
+                    Console.WriteLine(output);
+                    writer.WriteLine(output);
+                }
+            }
+        }
+    }
+}

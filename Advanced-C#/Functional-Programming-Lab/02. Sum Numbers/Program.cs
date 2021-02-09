eb746ff00797d9453afb0fc217aea8c6d6244f3c
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-02-09 08:03:38 +0200

    added new problem

---
+++ Advanced-C#/Functional-Programming-Lab/02. Sum Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int count = nums.Length;
+            int sum = nums.Sum();
+
+            Console.WriteLine(count);
+            Console.WriteLine(sum);                                            
+        }
+    }
+}

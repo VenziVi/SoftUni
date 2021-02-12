2f555662145e65ae3a635627e49612c182a61157
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-02-12 08:13:48 +0200

    added new problem

---
+++ Advanced-C#/Functional-Programming-Exercises/03. Custom Min Function/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int[], int> minNum = num => 
+            {
+                int minNum = int.MaxValue;
+                foreach (var item in nums)
+                {
+                    if (item < minNum)
+                    {
+                        minNum = item;
+                    }
+                }
+
+                return minNum;
+            };
+
+            Console.WriteLine(minNum(nums));
+        }
+    }
+}

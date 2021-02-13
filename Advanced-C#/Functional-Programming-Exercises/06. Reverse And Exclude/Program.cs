a2f14a7c67fea458f36c8c6dfe9d19d912fde638
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-02-13 08:37:04 +0200

    added new problem

---
+++ Advanced-C#/Functional-Programming-Exercises/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int n = int.Parse(Console.ReadLine());
+
+            input.Reverse();
+            //input = input.Where(num => num % n != 0).ToList();
+            Func<int, bool> predicate = num => num % n != 0;
+            input = input.Where(predicate).ToList();
+            Action<List<int>> print = n => Console.WriteLine(string.Join(" ", n));
+            print(input);
+        }
+    }
+}

71c3035e5fe1f81953f66339f51d73fb2ca0e77f
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-02-18 22:48:23 +0200

    added new task

---
+++ Advanced-C#/Defining-Classes-Lab/04. Car Engine and Tires/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var tyres = new Tire[4]
+            {
+                new Tire(2012, 2.2),
+                new Tire(2012, 2.2),
+                new Tire(2012, 2.5),
+                new Tire(2012, 2.5)
+            };
+
+            Engine v12 = new Engine(225, 3.0);
+
+            Car lambo = new Car("lambo", "devil", 2020, 70, 25.0, v12, tyres);
+        }
+    }
+}

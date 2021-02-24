bfe0b8e55aaed6c0a6d5499671556a5835bf2c45
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-02-24 08:18:40 +0200

    added new task

---
+++ Advanced-C#/Custom-Doubly-Linked-List/Custom-Doubly-Linked-List/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomDoublyLinkedList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+            list.AddFirst(5);
+            list.AddFirst(8);
+            list.AddFirst(3);
+            list.AddFirst(4);
+            list.AddFirst(9);
+            list.RemoveFirst();
+            list.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}

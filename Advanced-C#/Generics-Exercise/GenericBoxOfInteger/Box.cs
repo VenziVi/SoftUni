37986817443cded0ed880625eb1620dd1dd763f9
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-02-27 09:23:16 +0200

    added new task

---
+++ Advanced-C#/Generics-Exercise/GenericBoxOfInteger/Box.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericBoxOfInteger
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            string print = $"{Value.GetType()}: {Value}";
+            return print;
+        }
+    }
+}
+

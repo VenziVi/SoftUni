987ef163ab9aebe6d597d4344b4062b37087df90
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-03-04 08:10:30 +0200

    added new task

---
+++ Advanced-C#/Iterators-and-Comparators-Exercise/02Collection/ListyIterator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> list;
+        private int index;
+        public ListyIterator(List<T> list)
+        {
+            this.list = list;
+            this.index = 0;
+        }
+        // 10 20 30
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.index >= this.list.Count)
+            {
+                throw new IndexOutOfRangeException("Invalid Operation!");
+            }
+
+            Console.WriteLine(list[index]);
+        }
+
+        public bool HasNext()
+        {
+            if (this.index == this.list.Count - 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in this.list)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        internal void PrintAll()
+        {
+            if (list.Count > 0)
+            {
+                Console.WriteLine(string.Join(" ", list));
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Invalid Operation!");
+            }
+        }
+    }
+}
---
+++ Advanced-C#/Iterators-and-Comparators-Exercise/02Collection/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> inputs = new List<string>();
+            ListyIterator<string> list = new ListyIterator<string>(inputs);
+
+            string command = Console.ReadLine();
+
+            while (command != "END")
+            {
+                string[] splited = command.Split();
+                string cmdArgs = splited[0];
+
+                switch (cmdArgs)
+                {
+                    case "Create":
+                        string[] input = command.Split().Skip(1).ToArray();
+                        for (int i = 0; i < input.Length; i++)
+                        {
+                            inputs.Add(input[i]);
+                        }
+                        break;
+
+                    case "Move":
+                        Console.WriteLine(list.Move());
+                        break;
+
+                    case "Print":
+                        try
+                        {
+                            list.Print();
+                        }
+                        catch (IndexOutOfRangeException e)
+                        {
+
+                            Console.WriteLine(e.Message); ;
+                        }
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(list.HasNext());
+                        break;
+
+                    case "PrintAll":
+                        try
+                        {
+                            list.PrintAll();
+                        }
+                        catch (IndexOutOfRangeException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}

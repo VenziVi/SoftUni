c97263e7aa21aa61e4e406609265aff5596fb858
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-03-18 07:54:24 +0200

    added new task

---
+++ OOP-C#/Inheritance-Lab/4RandomList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+            list.Add("joro");
+            list.Add("koko");
+            list.Add("moko");
+            list.Add("boko");
+            list.Add("skip");
+
+            Console.WriteLine(list.RandomString());           
+        }
+    }
+}

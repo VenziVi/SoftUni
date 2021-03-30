a18b1ca423e109648cc513701567bd5ed0078b91
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-03-30 07:52:53 +0300

    added new task

---
+++ OOP-C#/InterfacesAndAbstraction-Exercise/BirthdayCelebrations/Citizen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    class Citizen : IPerson, IIdentifiable, IBirthdate
+    {
+        public Citizen(string name, int age, string id, string birthDate)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.BirthDate = birthDate;
+        }
+        
+        public string Name { get; private set; }
+
+        public int Age { get; private set; }
+
+        public string Id { get; private set; }
+
+        public string BirthDate { get; private set; }
+    }
+}
---
+++ OOP-C#/InterfacesAndAbstraction-Exercise/BirthdayCelebrations/Pet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BirthdayCelebrations
+{
+    class Pet : IPet, IBirthdate
+    {
+        public Pet(string name, string birthDate)
+        {
+            this.Name = name;
+            this.BirthDate = birthDate;
+        }
+        public string Name { get; private set; }
+
+        public string BirthDate { get; private set; }
+    }
+}

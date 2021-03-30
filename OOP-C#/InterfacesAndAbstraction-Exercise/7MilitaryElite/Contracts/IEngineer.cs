0434c78f21e7dc416bb7305aed9945609c1707a9
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-03-30 07:52:30 +0300

    added new task

---
+++ OOP-C#/InterfacesAndAbstraction-Exercise/7MilitaryElite/Contracts/IEngineer.cs
@@ -0,0 +1,12 @@
+﻿using _7MilitaryElite.Enums;
+using System.Collections.Generic;
+
+namespace _7MilitaryElite.Contracts
+{
+    public interface IEngineer : ISpecialisedSoldier
+    {
+        IReadOnlyCollection<IRepair> Repairs { get; }
+
+        void AddRepair(IRepair repair);
+    }
+}
---
+++ OOP-C#/InterfacesAndAbstraction-Exercise/7MilitaryElite/Contracts/IMission.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using _7MilitaryElite.Enums;
+
+namespace _7MilitaryElite.Contracts
+{
+    public interface IMission
+    {
+        string CodeName { get; }
+
+        MissionState MissionState { get; }
+
+        void CompleteMission();
+    }
+}

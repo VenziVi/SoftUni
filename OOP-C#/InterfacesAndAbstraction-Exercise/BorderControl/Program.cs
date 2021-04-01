ed5ba92d05a9c24c6f7c77b14c36317c9941c01e
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-04-01 08:14:46 +0300

    added new task

---
+++ OOP-C#/InterfacesAndAbstraction-Exercise/BorderControl/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<IIdentifiable> identifibles = new List<IIdentifiable>();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "End")
+                {
+                    break;
+                }
+
+                var parts = line.Split();
+
+                if (parts.Length == 3)
+                {
+                    var name = parts[0];
+                    var age = int.Parse(parts[1]);
+                    var id = parts[2];
+                    identifibles.Add(new Citizen(name, age, id));
+                }
+                else
+                {
+                    var model = parts[0];
+                    var id = parts[1];
+                    identifibles.Add(new Robot(model, id));
+                }
+            }
+
+            var digits = Console.ReadLine();
+
+            var realId = identifibles.Where(i => i.Id.EndsWith(digits)).ToList();
+
+            foreach (var id in realId)
+            {
+                Console.WriteLine(id.Id);
+            }
+        }
+    }
+}
---
+++ OOP-C#/InterfacesAndAbstraction-Exercise/BorderControl/Robot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    class Robot : IRobot, IIdentifiable
+    {
+        public Robot(string Model, string id)
+        {
+            this.Model = Model;
+            this.Id = id;
+        }
+        public string Model { get; private set; }
+
+        public string Id { get; private set; }
+    }
+}

20044b02e7604106352ef7026f71328f36b8146c
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-04-07 07:35:19 +0300

    added new task

---
+++ OOP-C#/Polymorphism-Lab/Shapes/Rectangle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public double Height 
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("invalid height!");
+                }
+                this.height = value;
+            }
+        }
+
+        public double Width 
+        {
+            get => this.width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("invalid width!");
+                }
+                this.width = value;
+            }
+        }
+
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (Width + Height);
+        }
+
+        public override string Draw()
+        {
+            return base.Draw() + this.GetType().Name;
+        }   
+
+    }
+}

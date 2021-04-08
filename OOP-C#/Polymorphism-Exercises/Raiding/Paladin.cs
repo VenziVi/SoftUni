1af948139e2699a510b91683c94966790ab61c6d
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-04-08 18:54:13 +0300

    added new task

---
+++ OOP-C#/Polymorphism-Exercises/Raiding/Paladin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Paladin : BaseHero
+    {
+        private const int PaladinBasePower = 100;
+        public Paladin(string name) 
+            : base(name, PaladinBasePower)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{nameof(Paladin)} - {Name} healed for {Power}";
+        }
+    }
+}
---
+++ OOP-C#/Polymorphism-Exercises/Raiding/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public class Warrior : BaseHero
+    {
+        private const int WarriorBasePower = 100;
+        public Warrior(string name) 
+            : base(name, WarriorBasePower)
+        {
+        }
+
+        public override string CastAbility()
+        {
+            return $"{nameof(Warrior)} - {Name} hit for {Power} damage";
+        }
+    }
+}

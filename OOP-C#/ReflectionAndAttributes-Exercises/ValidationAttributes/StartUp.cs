1c2f5c245d0c1f95333eece2f92d4cdeb9eb7dbd
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-04-15 07:35:29 +0300

    added new task

---
+++ OOP-C#/ReflectionAndAttributes-Exercises/ValidationAttributes/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ValidationAttributes
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var person = new Person
+             (
+                 "venci",
+                 23
+             );
+
+            bool isvalidentity = Validator.IsValid(person);
+
+            Console.WriteLine(isvalidentity);
+        }
+    }
+}

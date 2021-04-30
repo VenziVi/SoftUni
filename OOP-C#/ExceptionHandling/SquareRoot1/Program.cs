a9823c3ba5438f312fe4be8f86feb9cf78d8a2b1
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-04-30 07:43:20 +0300

    added new task

---
+++ OOP-C#/ExceptionHandling/SquareRoot1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace SquareRoot1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < 0)
+                {
+                    throw new ArgumentException("Invalid Number!");
+                }
+                if (num == 0)
+                {
+                    throw new ArgumentException("Number cannot be zero!");
+                }
+
+                Console.WriteLine(num * num);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException fEx)
+            {
+                Console.WriteLine(fEx.GetType().Name);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}

8dc45f8203e1df963556c8449447938b07a37321
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-04-30 07:42:53 +0300

    added new task

---
+++ OOP-C#/ExceptionHandling/SquareRoot/Number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SquareRoot
+{
+    class Number
+    {
+        private int num;
+
+        public Number(int num)
+        {
+            Num = num;
+        }
+        public int Num 
+        {
+            get => num;
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid number!");
+                }
+
+                num = value;
+            }
+        }
+
+        public int GetSquare()
+        {
+            return num * num;
+        }
+    }
+}
---
+++ OOP-C#/ExceptionHandling/SquareRoot/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Number number = new Number(n);
+                Console.WriteLine(number.GetSquare());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good bye.");
+            }
+        }
+    }
+}

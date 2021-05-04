f797ac1dd8bd3c770f5ae20eb072c5a5dbb4d7b7
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-05-04 19:39:22 +0300

    added SimpleSnake

---
+++ SimpleSnake/SimpleSnake/Core/Engine.cs
@@ -0,0 +1,131 @@
+﻿using SimpleSnake.Enums;
+using SimpleSnake.GameObjects;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using Point = SimpleSnake.GameObjects.Point;
+
+namespace SimpleSnake.Core
+{
+    public class Engine
+    {
+        private Snake snake;
+        private Wall wall;
+        private double sleepTime;
+        private Direction direction;
+        private Point[] pointsOfDirection;
+
+        public Engine(Wall wall, Snake snake)
+        {
+            this.wall = wall;
+            this.snake = snake;
+            this.sleepTime = 100;
+            this.pointsOfDirection = new Point[4];
+        }
+
+        private void CreateDirections()
+        {
+            this.pointsOfDirection[0] = new Point(1, 0);
+            this.pointsOfDirection[1] = new Point(-1, 0);
+            this.pointsOfDirection[2] = new Point(0, 1);
+            this.pointsOfDirection[3] = new Point(0, -1);
+        }
+
+        private void GetNextDirection()
+        {
+            ConsoleKeyInfo userInput = Console.ReadKey();
+
+            if (userInput.Key == ConsoleKey.LeftArrow)
+            {
+                if (direction != Direction.Right)
+                {
+                    direction = Direction.Left;
+                }
+            }
+            else if (userInput.Key == ConsoleKey.RightArrow)
+            {
+                if (direction != Direction.Left)
+                {
+                    direction = Direction.Right;
+                }
+            }
+            else if (userInput.Key == ConsoleKey.UpArrow)
+            {
+                if (direction != Direction.Down)
+                {
+                    direction = Direction.Up;
+                }
+            }
+            else if (userInput.Key == ConsoleKey.DownArrow)
+            {
+                if (direction != Direction.Up)
+                {
+                    direction = Direction.Down;
+                }
+            }
+
+            Console.CursorVisible = false;
+
+             bool isMoving = snake.IsMoving(this.pointsOfDirection[(int)direction]);
+
+            if (!isMoving)
+            {
+                AskForRestart();
+            }
+        }
+
+        public void AskForRestart()
+        {
+            int leftX = this.wall.LeftX + 1;
+            int topY = 3;
+
+            Console.SetCursorPosition(leftX, topY);
+            Console.Write("Would you like to continue? y/n");
+
+            string input = Console.ReadLine();
+
+            if (input == "y")
+            {
+                Console.Clear();
+                StartUp.Main();
+            }
+            else
+            {
+                StopGame();
+            }
+        }
+
+        private void StopGame()
+        {
+            Console.SetCursorPosition(20, 10);
+            Console.Write("Game Over!");
+            Environment.Exit(0);
+        }
+
+        public void Run()
+        {
+            this.CreateDirections();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    GetNextDirection();
+                }
+
+                bool isMoving = snake.IsMoving(this.pointsOfDirection[(int)direction]);
+
+                if (!isMoving)
+                {
+                    AskForRestart();
+                }
+
+                sleepTime -= 0.1;
+
+                Thread.Sleep((int)sleepTime);
+            }
+        }
+    }
+}

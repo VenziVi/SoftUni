1729617146c56906f05cf392172090e36b0deb2a
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-05-06 12:22:59 +0300

    added Phonebook

---
+++ Phonebook/Phonebook/Controllers/PhonebookController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phonebook.Models;
+
+namespace Phonebook.Controllers
+{
+    public class PhonebookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DataContext.Contacts);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+                DataContext.Contacts.Add(model);
+            }
+            else
+            {
+                TempData["Error"] = "Невалидни данни!";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}

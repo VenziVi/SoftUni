5307450f2ddae1b01a2bdc5edc5a126b4d7c3580
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-05-08 09:38:41 +0300

    added new task

---
+++ OOP-C#/DesignPatterns/TemplatePattern/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TemplatePattern
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            TwelvGrain twelvGrain = new TwelvGrain();
+            twelvGrain.make();
+
+            Console.WriteLine("----------------------------");
+            
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.make();
+        }
+    }
+}
---
+++ OOP-C#/DesignPatterns/TemplatePattern/TwelvGrain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplatePattern
+{
+    public class TwelvGrain : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Mixing ingredians for 12-grain bread!");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking 12-grain bread (20 min)!");
+        }
+
+    }
+}

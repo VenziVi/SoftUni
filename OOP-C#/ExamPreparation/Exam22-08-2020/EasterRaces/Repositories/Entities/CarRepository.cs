7aa24955d1bb2d4db3691324e050c59a240f23e5
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-05-17 07:19:26 +0300

    added new problem

---
+++ OOP-C#/ExamPreparation/Exam22-08-2020/EasterRaces/Repositories/Entities/CarRepository.cs
@@ -0,0 +1,33 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Cars.Entities;
+using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class CarRepository : IRepository<ICar>
+    {
+        private List<ICar> cars;
+
+        public CarRepository()
+        {
+            cars = new List<ICar>();
+        }
+
+        public void Add(ICar model) 
+            => this.cars.Add(model);
+
+        public IReadOnlyCollection<ICar> GetAll() 
+            => this.cars.ToArray();
+
+
+        public ICar GetByName(string name) 
+            => this.cars.FirstOrDefault(c => c.Model == name);
+
+        public bool Remove(ICar model) 
+            => this.cars.Remove(model);
+    }
+}
---
+++ OOP-C#/ExamPreparation/Exam22-08-2020/EasterRaces/Repositories/Entities/DriverRepository.cs
@@ -0,0 +1,33 @@
+﻿using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Models.Drivers.Entities;
+using EasterRaces.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasterRaces.Repositories.Entities
+{
+    public class DriverRepository : IRepository<IDriver>
+    {
+        private List<IDriver> drivers;
+
+        public DriverRepository()
+        {
+            drivers = new List<IDriver>();
+        }
+
+        public void Add(IDriver model)
+            => this.drivers.Add(model);
+
+        public IReadOnlyCollection<IDriver> GetAll()
+            => this.drivers.ToArray();
+
+
+        public IDriver GetByName(string name)
+            => this.drivers.FirstOrDefault(d => d.Name == name);
+
+        public bool Remove(IDriver model)
+            => this.drivers.Remove(model);
+    }
+}

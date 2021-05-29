aaa37000617fcc127c39abc86bc4d3bc3c9ab352
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-05-29 07:32:19 +0300

    added new problem

---
+++ JavaScript-Advanced/Syntax-Functions-Statements-Lab/StringLength.js
@@ -0,0 +1,8 @@
+function strLen(a, b, c){
+    let total = a.length + b.length + c.length;
+    console.log(total);
+    console.log(Math.floor(total / 3));
+}
+
+strLen('chocolate', 'ice cream', 'cake');
+strLen('pasta', '5', '22.3');

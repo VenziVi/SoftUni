0b06ee450bf652deda604f07bcee05aee9c22726
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-06-13 09:38:34 +0300

    added new problem

---
+++ JavaScript-Advanced/Arrays-Exercise/RotateArray.js
@@ -0,0 +1,11 @@
+function solve(arr, n){   
+    for(let i = 0; i < n; i++){
+        let lastElement = arr.pop();
+        arr.unshift(lastElement);
+    }
+
+    console.log(arr.join(' '));
+}
+
+solve(['1', '2', '3', '4'], 2);
+solve(['Banana', 'Orange', 'Coconut', 'Apple'], 15);

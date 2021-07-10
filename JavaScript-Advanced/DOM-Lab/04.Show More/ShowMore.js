845d0a5d610dcd574632fbea4348f6743835603a
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-07-10 08:32:53 +0300

    added new problem

---
+++ JavaScript-Advanced/DOM-Lab/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let getTextElemnt = document.getElementById('text');
+    getTextElemnt.style.display = "inline";
+    let ReadMoreElement = document.getElementById('more');
+    ReadMoreElement.style.display = "none";
+}

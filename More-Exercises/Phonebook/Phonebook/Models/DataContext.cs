565869f0a1499bbf72e7a557c626d7f9c5ba8d18
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-07-14 07:52:21 +0300

    calculator moved to more-exercises

---
+++ More-Exercises/Phonebook/Phonebook/Models/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Phonebook.Models
+{
+    public class DataContext
+    {
+        public static List<Contact> Contacts { get; set; }
+    }
+}
---
+++ More-Exercises/RandomGenerator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RandomGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> participants = new List<string>();
+
+            using (StreamReader file = new StreamReader("../../../participants.txt"))
+            {
+                int counter = 0;
+                string line;
+
+                while ((line = file.ReadLine()) != null)
+                {
+                    counter++;
+                    participants.Add(line);
+                    
+                }
+
+            }
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                int num = rnd.Next(1, participants.Count);
+
+                Console.WriteLine(participants[num]);
+
+                participants.Remove(participants[num]);
+            }
+            
+        }
+    }
+}

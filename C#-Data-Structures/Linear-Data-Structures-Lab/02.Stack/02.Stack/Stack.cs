0e8bd5d11a579ffd895f0988cb7f62b803f022d7
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-08-26 07:51:11 +0300

    added stack implementation

---
+++ C#-Data-Structures/Linear-Data-Structures-Lab/02.Stack/02.Stack/Stack.cs
@@ -0,0 +1,85 @@
+﻿namespace Problem02.Stack
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class Stack<T> : IAbstractStack<T>
+    {
+        private Node<T> top;
+
+        public int Count { get; private set; }
+
+        public bool Contains(T item)
+        {
+            var currItem = this.top;
+
+            while (currItem != null)
+            {
+                if (currItem.Value.Equals(item))
+                {
+                    return true;
+                }
+
+                currItem = currItem.Previous;
+            }
+
+            return false;
+        }
+
+        public T Peek()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return this.top.Value;
+        }
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var item = this.top;
+            this.top = top.Previous;
+            this.Count--;
+            return item.Value;
+        }
+
+        public void Push(T item)
+        {
+            var newItem = new Node<T>();
+            newItem.Value = item;
+
+            if (this.Count == 0)
+            {
+                this.top = newItem;
+                this.Count++;
+                return;
+            }
+
+            newItem.Previous = this.top;
+            this.top = newItem;
+            this.Count++;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var item = top;
+
+            while (item != null)
+            {
+                yield return item.Value;
+
+                item = item.Previous;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() 
+            => GetEnumerator();
+    }
+}

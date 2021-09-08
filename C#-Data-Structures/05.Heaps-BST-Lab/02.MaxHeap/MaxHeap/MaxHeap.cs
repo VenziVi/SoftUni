f1129248c6c34abc2eb3a19710beca7230c519fb
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-09-08 09:25:20 +0300

    added MaxHeap

---
+++ C#-Data-Structures/05.Heaps-BST-Lab/02.MaxHeap/MaxHeap/MaxHeap.cs
@@ -0,0 +1,47 @@
+﻿namespace _02.MaxHeap
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MaxHeap<T> : IAbstractHeap<T>
+        where T : IComparable<T>
+    {
+        private List<T> heap;
+
+        public MaxHeap()
+        {
+            this.heap = new List<T>();
+        }
+
+        public int Size { get { return this.heap.Count; } }
+
+        public void Add(T element)
+        {
+            this.heap.Add(element);
+            Heapify(this.heap.Count - 1);
+        }
+        
+        public T Peek()
+        {
+            if (this.heap.Count == 0)
+            {
+                throw new InvalidOperationException();
+            }
+            return heap[0];
+        }
+
+        private void Heapify(int index)
+        {
+            if (index == 0) return;
+            int parentIndex = (index - 1) / 2;
+
+            if (heap[index].CompareTo(heap[parentIndex]) > 0)
+            {
+                var temp = heap[index];
+                heap[index] = heap[parentIndex];
+                heap[parentIndex] = temp;
+                Heapify(parentIndex);
+            }
+        }
+    }
+}

7f83f97a3a705e755d732bb4b75cd81570c7249c
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-09-11 09:16:49 +0300

    added MinHeap

---
+++ C#-Data-Structures/06.Heaps-BST-Exercise/03.MinHeap/MinHeap/MinHeap.cs
@@ -0,0 +1,60 @@
+﻿namespace _03.MinHeap
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class MinHeap<T> : IAbstractHeap<T>
+        where T : IComparable<T>
+    {
+        private List<T> elements;
+
+        public MinHeap()
+        {
+            this.elements = new List<T>();
+        }
+
+        public int Size => elements.Count;
+
+        public T Dequeue()
+        {
+            ValidateIfEmpty();
+
+            var result = elements[0];
+            elements.RemoveAt(0);
+
+            return result;
+        }
+
+        public void Add(T element)
+        {
+            elements.Add(element);
+
+
+            int startIndex = this.Size - 1;
+
+            while (startIndex != 0 && elements[startIndex].CompareTo(elements[startIndex - 1]) == -1)
+            {
+                var temp = elements[startIndex - 1];
+                elements[startIndex - 1] = elements[startIndex];
+                elements[startIndex] = temp;
+
+                startIndex--;
+            }
+        }
+
+        public T Peek()
+        {
+            ValidateIfEmpty();
+
+            return elements[0];
+        }
+
+        private void ValidateIfEmpty()
+        {
+            if (this.Size == 0)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}

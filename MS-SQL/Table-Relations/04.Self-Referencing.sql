caca65d6964ab5fafadca8c123bb567292827038
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-10-03 10:04:25 +0300

    self referencing

---
+++ MS-SQL/Table-Relations/04.Self-Referencing.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Teachers (
+	TeacherID INT PRIMARY KEY IDENTITY(101, 1),
+	[Name] VARCHAR(20) NOT NULL,
+	ManagerID INT FOREIGN KEY REFERENCES Teachers(TeacherID)
+)
+
+INSERT INTO Teachers
+VALUES ('Jhon', NULL),
+       ('Maya', 106),
+	   ('Silvia', 106),
+	   ('Ted', 105),
+	   ('Mark', 101),
+	   ('Greta', 101)

1a3b34493b1deae5c14fb4c2637624ae8c41be80
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-10-17 11:10:27 +0300

    3rd highest salary

---
+++ MS-SQL/Indices-And-Data-Aggregation/18.3rd-Highest-Salary.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT DepartmentID,
+       Salary
+  FROM (
+       SELECT DepartmentID,
+              Salary,
+              DENSE_RANK() OVER(PARTITION BY DepartmentID ORDER BY Salary DESC) AS SalaryRank
+         FROM Employees
+         ) AS RankedSalary
+ WHERE SalaryRank = 3

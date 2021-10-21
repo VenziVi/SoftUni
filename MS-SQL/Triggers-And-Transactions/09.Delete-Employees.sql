3acd8fa7bf49b5f9212ad2e4ca186eb07ac3df5d
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-10-21 07:41:33 +0300

    delete employee

---
+++ MS-SQL/Triggers-And-Transactions/09.Delete-Employees.sql
@@ -0,0 +1,7 @@
+CREATE TRIGGER tr_LogDeletedEmployees
+ON Employees FOR DELETE
+AS
+BEGIN
+	INSERT INTO Deleted_Employees
+	SELECT FirstName,LastName,MiddleName,JobTitle,DepartmentID,Salary FROM deleted
+END

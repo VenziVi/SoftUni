a9bd4da00523032344d7d216683c89b9395864db
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-10-24 11:45:04 +0300

    get villain names

---
+++ Entity-Framework-Core/01.ADO.NET/02.VillainNames/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _02.VillainNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=True");
+
+            connection.Open();
+
+            var sqlCommand = new SqlCommand(Query.query, connection);
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            using (sqlDataReader)
+            {
+                while (sqlDataReader.Read())
+                {
+                    Console.WriteLine($"{sqlDataReader["Name"]} - {sqlDataReader["Count"]}");
+                }
+            }
+        }
+        
+    }
+}
---
+++ Entity-Framework-Core/01.ADO.NET/02.VillainNames/Query.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.VillainNames
+{
+    public static class Query
+    {
+        public static string query = @"SELECT v.Name AS Name,COUNT(*) AS [Count]
+                                	FROM MinionsVillains as mv
+                                	JOIN Villains AS v ON v.Id = mv.VillainId
+                                	JOIN Minions AS m ON m.Id = mv.MinionId
+                                GROUP BY v.Name
+                                HAVING COUNT(*) >= 3
+                                ORDER BY COUNT(*) DESC";
+    }
+}

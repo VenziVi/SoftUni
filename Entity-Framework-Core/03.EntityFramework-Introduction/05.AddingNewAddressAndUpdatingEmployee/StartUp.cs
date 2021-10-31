1bd80272da16bd44a9120e200d697198dd453d7b
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-10-31 09:55:15 +0200

    adding new address

---
+++ Entity-Framework-Core/03.EntityFramework-Introduction/05.AddingNewAddressAndUpdatingEmployee/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Text;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(AddNewAddressToEmployee(context));
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+
+            Address newAddress = new Address();
+            newAddress = new Address
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            Employee nakovEmployee = context
+                .Employees
+                .FirstOrDefault(e => e.LastName == "Nakov");
+
+            nakovEmployee.Address = newAddress;
+
+            context.SaveChanges();
+
+            var employees = context
+                .Employees
+                .OrderByDescending(e => e.AddressId)
+                .Select(e => new
+                {
+                    e.Address.AddressText
+                })
+                .Take(10)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var emp in employees)
+            {
+                sb.AppendLine($"{emp.AddressText}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

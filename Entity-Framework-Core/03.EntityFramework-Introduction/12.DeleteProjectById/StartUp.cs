e1d27ca06fa9ed6975e154984058aa259dc69552
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-02 07:36:04 +0200

    delete project by id

---
+++ Entity-Framework-Core/03.EntityFramework-Introduction/12.DeleteProjectById/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(DeleteProjectById(context));
+        }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var project = context.Projects
+                .Include(p => p.EmployeesProjects)
+                .SingleOrDefault(p => p.ProjectId == 2);
+
+            if (project != null)
+            {
+                foreach (var projectType in project.EmployeesProjects)
+                {
+                    context.EmployeesProjects.Remove(projectType);
+                }
+            }
+
+            context.Projects.Remove(project);
+
+            context.SaveChanges();
+
+            var projects = context.Projects.Take(10).ToArray();
+
+            foreach (var prj in projects)
+            {
+                sb.AppendLine($"{prj.Name}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}

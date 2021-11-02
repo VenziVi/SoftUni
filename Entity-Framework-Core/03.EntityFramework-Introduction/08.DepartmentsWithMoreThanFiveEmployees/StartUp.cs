ae7219d7197e92089f2794b7f2784056e4c246bf
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-02 07:33:16 +0200

    department with more than five employee

---
+++ Entity-Framework-Core/03.EntityFramework-Introduction/08.DepartmentsWithMoreThanFiveEmployees/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetDepartmentsWithMoreThan5Employees(context));
+        }
+
+        public static string GetDepartmentsWithMoreThan5Employees(SoftUniContext context)
+        {
+            var departments = context
+                .Departments
+                .Where(d => d.Employees.Count > 5)
+                .Include(x => x.Manager)
+                .Include(x => x.Employees)
+                .OrderBy(d => d.Employees.Count)
+                .ThenBy(d => d.Name)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var department in departments)
+            {
+                sb.AppendLine($"{department.Name} – {department.Manager.FirstName} {department.Manager.LastName}");
+
+                var employees = department.Employees.OrderBy(e => e.FirstName).ThenBy(e => e.LastName);
+
+                foreach (var emp in employees)
+                {
+                    sb.AppendLine($"{emp.FirstName} {emp.LastName} - {emp.JobTitle}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}

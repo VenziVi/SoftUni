d0bedb5e96b60e8b0a270e42eccfa88683b85b58
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-02 07:33:50 +0200

    find latest ten projects

---
+++ Entity-Framework-Core/03.EntityFramework-Introduction/09.FindLatestTenProjects/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net.Http.Headers;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetLatestProjects(context));
+        }
+
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var projects = context
+                .Projects
+                .Select(p => new
+                {
+                    p.Name,
+                    p.Description,
+                    p.StartDate
+                })
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .OrderBy(p => p.Name)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var project in projects)
+            {
+                sb.AppendLine($"{project.Name}");
+                sb.AppendLine($"{project.Description}");
+                sb.AppendLine($"{project.StartDate.ToString("M/d/yyyy h:mm:ss tt")}");
+
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}

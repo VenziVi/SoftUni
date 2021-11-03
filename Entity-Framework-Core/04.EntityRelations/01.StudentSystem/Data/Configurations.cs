ea8e1fcc9709929c098ffc8c727b3fc1f93f499a
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-03 07:43:08 +0200

    added student system

---
+++ Entity-Framework-Core/04.EntityRelations/01.StudentSystem/Data/Configurations.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_StudentSystem.Data.Models
+{
+    public static class Configurations
+    {
+        public static string CONNECTION_STRING = "Server=.;Database=StudentSystem;Integrated Security=true";
+    }
+}
---
+++ Entity-Framework-Core/04.EntityRelations/01.StudentSystem/Data/StudentSystemContext.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using P01_StudentSystem.Data.Models;
+
+namespace P01_StudentSystem.Data
+{
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+        {
+        }
+
+        public StudentSystemContext([NotNull] DbContextOptions options) 
+            : base(options)
+        
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configurations.CONNECTION_STRING);
+            }
+        }
+
+        public virtual DbSet<Course> Courses { get; set; }
+
+        public virtual DbSet<Homework> HomeworkSubmissions { get; set; }
+
+        public virtual DbSet<Resource> Resources { get; set; }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        public virtual DbSet<StudentCourse> StudentCourses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<StudentCourse>(e =>
+            {
+                e.HasKey(sc => new {sc.StudentId, sc.CourseId});
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

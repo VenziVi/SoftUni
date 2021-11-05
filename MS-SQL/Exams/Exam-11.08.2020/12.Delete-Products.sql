4709bf53e56fe259d07cf049370dae17be8ae4d3
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-05 07:50:47 +0200

    delete products

---
+++ MS-SQL/Exams/Exam-11.08.2020/12.Delete-Products.sql
@@ -0,0 +1,11 @@
+ CREATE TRIGGER dbo.tr_OnDelete
+ ON Products INSTEAD OF DELETE
+ AS
+	DELETE FROM Feedbacks
+	WHERE ProductId IN (SELECT Id FROM deleted)
+
+	DELETE FROM ProductsIngredients
+	WHERE ProductId IN (SELECT Id FROM deleted)
+
+	DELETE FROM Products
+	WHERE Id IN (SELECT Id FROM deleted)

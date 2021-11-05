1c56501ad062f84cd0b180fea73055c8f6c5fa33
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-05 07:48:20 +0200

    albums info

---
+++ Entity-Framework-Core/05.LINQ/02.AlbumsInfo/StartUp.cs
@@ -0,0 +1,83 @@
+﻿namespace MusicHub
+{
+    using System;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            MusicHubDbContext context = 
+                new MusicHubDbContext();
+
+            DbInitializer.ResetDatabase(context);
+
+            //Test your solutions here
+
+            Console.WriteLine(ExportAlbumsInfo(context, 9));
+        }
+
+        public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
+        {
+            var albums = context
+                .Albums
+                .ToArray()
+                .Where(a => a.ProducerId == producerId)
+                .OrderByDescending(a => a.Price)
+                .Select(a => new
+                {
+                    albumName = a.Name,
+                    ReleaseDate = a.ReleaseDate.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture),
+                    ProducerName = a.Producer.Name,
+                    songs = a.Songs
+                    .ToArray()
+                    .Select(s => new
+                    {
+                        songName = s.Name,
+                        songPrice = s.Price,
+                        songWriterName = s.Writer.Name
+                    })
+                    .OrderByDescending(s => s.songName)
+                    .ThenBy(s => s.songWriterName)
+                    .ToArray(),
+                    totalAlbumPrice = a.Price
+                })
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var album in albums)
+            {
+                sb
+                    .AppendLine($"-AlbumName: {album.albumName}")
+                    .AppendLine($"-ReleaseDate: {album.ReleaseDate}")
+                    .AppendLine($"-ProducerName: {album.ProducerName}")
+                    .AppendLine("-Songs:");
+
+                int i = 1;
+
+                foreach (var song in album.songs)
+                {
+                    sb
+                        .AppendLine($"---#{i++}")
+                        .AppendLine($"---SongName: {song.songName}")
+                        .AppendLine($"---Price: {song.songPrice:f2}")
+                        .AppendLine($"---Writer: {song.songWriterName}");
+                }
+
+                sb.AppendLine($"-AlbumPrice: {album.totalAlbumPrice:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Entity-Framework-Core/05.LINQ/02.AlbumsInfo/ValidationsConstraint.cs
@@ -0,0 +1,20 @@
+﻿namespace MusicHub
+{
+    public static class ValidationsConstraint
+    {
+        //Song
+        public const int SongNameMaxLength = 20;
+
+        //Album
+        public const int AlbumNameMaxLength = 40;
+
+        //Performer
+        public const int PerformerNameMaxLength = 20;
+
+        //Producer
+        public const int ProducerNameMaxLength = 20;
+
+        //Writer
+        public const int WriterNameMaxLength = 20;
+    }
+}

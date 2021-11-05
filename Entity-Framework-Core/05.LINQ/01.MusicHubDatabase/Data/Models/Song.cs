b71260ba425a3f2beb19c141fe9e38e0d3b6f939
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-05 07:47:45 +0200

    musicHub Database

---
+++ Entity-Framework-Core/05.LINQ/01.MusicHubDatabase/Data/Models/Song.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MusicHub.Data.Models.Enums;
+
+namespace MusicHub.Data.Models
+{
+    public class Song
+    {
+        public Song()
+        {
+            this.SongPerformers = new HashSet<SongPerformer>();
+            this.Performers = new HashSet<Performer>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(ValidationsConstraint.SongNameMaxLength)]
+        public string Name { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [ForeignKey(nameof(Album))]
+        public int? AlbumId { get; set; }
+        public virtual Album Album { get; set; }
+
+        [ForeignKey(nameof(Writer))]
+        public int WriterId { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        public decimal Price { get; set; }
+
+        public virtual ICollection<SongPerformer> SongPerformers { get; set; }
+
+        public virtual ICollection<Performer> Performers { get; set; }
+    }
+}

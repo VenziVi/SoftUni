b100a1ca094137ddb36f10b9795770d0c8791def
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-10 07:42:28 +0200

    age restriction query

---
+++ Entity-Framework-Core/06.AdvancedQuerying/01.AgeRestriction/BookShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            DbInitializer.ResetDatabase(db);
+        }
+
+        public static string GetBooksByAgeRestriction(BookShopContext context, string command)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            AgeRestriction restriction = Enum.Parse<AgeRestriction>(command, true);
+
+            var booksByAgeRestriction = context
+                .Books
+                .Where(b => b.AgeRestriction == restriction)
+                .OrderBy(b => b.Title)
+                .Select(b => b.Title)
+                .ToArray();
+
+            foreach (var title in booksByAgeRestriction)
+            {
+                sb.AppendLine(title);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

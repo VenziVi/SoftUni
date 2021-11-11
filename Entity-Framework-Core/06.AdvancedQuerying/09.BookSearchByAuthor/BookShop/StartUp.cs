01db97fa4107de18039e0d2a2a1ad120ff16704a
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-11 07:45:18 +0200

    book search by author

---
+++ Entity-Framework-Core/06.AdvancedQuerying/09.BookSearchByAuthor/BookShop/StartUp.cs
@@ -0,0 +1,42 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine(GetBooksByAuthor(db, input));
+        }
+
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context
+                .Books
+                .Where(b => b.Author.LastName.ToLower().StartsWith(input.ToLower()))
+                .Select(b => new { b.BookId, b.Title, b.Author.FirstName, b.Author.LastName})
+                .OrderBy(b => b.BookId)
+                .ToArray();
+                
+            foreach (var book in books)
+            {
+                sb.AppendLine($"{book.Title} ({book.FirstName} {book.LastName})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

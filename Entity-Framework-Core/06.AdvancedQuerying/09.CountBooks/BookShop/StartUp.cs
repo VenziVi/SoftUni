27f4deb9b1aeb1b7cd553c91645987f6deac288d
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-11 07:45:57 +0200

    count books

---
+++ Entity-Framework-Core/06.AdvancedQuerying/09.CountBooks/BookShop/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountBooks(db, input));
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var books = context
+                .Books
+                .Where(b => b.Title.Length > lengthCheck)
+                .Count();
+
+            return books;   
+        }
+    }
+}

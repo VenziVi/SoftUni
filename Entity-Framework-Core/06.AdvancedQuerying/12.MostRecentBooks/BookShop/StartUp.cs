a02f254b2d5a1f3d7878ae3ff2de833ea23b38aa
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-12 07:25:36 +0200

    most recent books query

---
+++ Entity-Framework-Core/06.AdvancedQuerying/12.MostRecentBooks/BookShop/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            //int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMostRecentBooks(db));
+        }
+
+        public static string GetMostRecentBooks(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var categorys = context
+                .Categories
+                .Select(c => new
+                {
+                    c.Name,
+                    Books = c.CategoryBooks.Select(b => new
+                    {
+                        b.Book.Title,
+                        b.Book.ReleaseDate
+                    })
+                    .OrderByDescending(b => b.ReleaseDate)
+                    .Take(3)
+                    .ToArray()
+                })
+                .OrderBy(c => c.Name)
+                .ToArray();
+
+                
+
+            foreach (var category in categorys)
+            {
+                sb.AppendLine($"--{category.Name}");
+
+                foreach (var book in category.Books)
+                {
+                    sb.AppendLine($"{book.Title} ({book.ReleaseDate.Value.Year})");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

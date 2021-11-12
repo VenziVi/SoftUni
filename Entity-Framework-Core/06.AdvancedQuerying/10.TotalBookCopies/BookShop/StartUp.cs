b2f90190258345569f7c17a50a9f924f235622f7
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-12 07:24:22 +0200

    total books copies query

---
+++ Entity-Framework-Core/06.AdvancedQuerying/10.TotalBookCopies/BookShop/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            //int input = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(CountCopiesByAuthor(db));
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var authors = context
+                .Authors
+                .Select(a => new
+                {
+                    a.FirstName,
+                    a.LastName,
+                    TotlaBooks = a.Books.Sum(b => b.Copies)
+                })
+                .OrderByDescending(a => a.TotlaBooks)
+                .ToArray();
+
+            foreach (var author in authors)
+            {
+                sb.AppendLine($"{author.FirstName} {author.LastName} - {author.TotlaBooks}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}

702a7fc83f2cf09c31e0506e005e317993ddb347
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-13 08:18:50 +0200

    increase price query

---
+++ Entity-Framework-Core/06.AdvancedQuerying/13.IncreasePrices/BookShop/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            //int input = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(IncreasePrices(db));
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+
+            var books = context
+                .Books
+                .Where(b => b.ReleaseDate.Value.Year < 2010);
+        
+            foreach (var book in books)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+ 
+        }
+    }
+}

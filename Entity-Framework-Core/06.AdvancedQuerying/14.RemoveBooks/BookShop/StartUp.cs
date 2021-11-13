4e53526599010827c1e742f7d76b240b99a9a626
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-13 08:19:59 +0200

    remove books with bulk

---
+++ Entity-Framework-Core/06.AdvancedQuerying/14.RemoveBooks/BookShop/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Linq;
+    using System.Text;
+    using Z.EntityFramework.Plus;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            //int input = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(IncreasePrices(db));
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+
+            var books = context
+                .Books
+                .Where(b => b.Copies < 4200)
+                .Delete();
+
+            return books;
+ 
+        }
+    }
+}

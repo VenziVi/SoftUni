a9adde769ac9049fde152c3e91ff47b0e9bdcefa
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-16 07:35:38 +0200

    cigars by taste

---
+++ MS-SQL/Exams/Exam-16.10.2021/06.Cigars-By-Taste.sql
@@ -0,0 +1,9 @@
+   SELECT c.Id,
+	      c.CigarName,
+	      c.PriceForSingleCigar,
+	      t.TasteType,
+	      t.TasteStrength
+     FROM Cigars AS c
+LEFT JOIN Tastes AS t ON c.TastId = t.Id
+    WHERE t.TasteType in ('Earthy', 'Woody')
+ ORDER BY c.PriceForSingleCigar DESC

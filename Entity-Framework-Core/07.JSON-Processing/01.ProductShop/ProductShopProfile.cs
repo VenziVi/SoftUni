c213bcaed48be45c29bd60dc60625f5818d1c909
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-18 07:39:56 +0200

    import users

---
+++ Entity-Framework-Core/07.JSON-Processing/01.ProductShop/ProductShopProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ProductShop.InputDtos;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class ProductShopProfile : Profile
+    {
+        public ProductShopProfile()
+        {
+            CreateMap<UserInputDto, User>();
+
+            CreateMap<ProductInputDto, Product>();
+
+            CreateMap<CategoriesInputDto, Category>();
+
+            CreateMap<CategoriesProductsInputDto, CategoryProduct>();
+        }
+    }
+}
---
+++ Entity-Framework-Core/07.JSON-Processing/01.ProductShop/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+using Newtonsoft.Json.Serialization;
+using ProductShop.InputDtos;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        private static IMapper mapper;
+        public static void Main(string[] args)
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            //var inputUsers = File.ReadAllText("../../../Datasets/users.json");
+            //var inputPeoducts = File.ReadAllText("../../../Datasets/products.json");
+            //var inputCategories = File.ReadAllText("../../../Datasets/categories.json");
+            //var inputCategoriesProducts = File.ReadAllText("../../../Datasets/categories-products.json");
+
+            //Console.WriteLine(ImportUsers(context, inputUsers));
+            //Console.WriteLine(ImportProducts(context, inputPeoducts)); 
+            //Console.WriteLine(ImportCategories(context, inputCategories));
+            //Console.WriteLine(ImportCategoryProducts(context, inputCategoriesProducts));
+            Console.WriteLine(GetUsersWithProducts(context));
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            IEnumerable<UserInputDto> users = JsonConvert.DeserializeObject<IEnumerable<UserInputDto>>(inputJson);
+
+            var mapperConfiguration = new MapperConfiguration(cfg => 
+            {
+                cfg.AddProfile<ProductShopProfile>();
+            });
+
+            mapper = new Mapper(mapperConfiguration);
+
+            var mappedUsers = mapper.Map<IEnumerable<User>>(users);
+
+            context.Users.AddRange(mappedUsers);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count()}";
+        }
+    }
+}

fdceef26691883791f713bfd96c6eb3455988561
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-11-27 08:27:22 +0200

    eee mails query

---
+++ MS-SQL/Exams/Exam-21.06.2020/05.EEE-Mails.sql
@@ -0,0 +1,10 @@
+  SELECT a.FirstName,
+         a.LastName,
+	     FORMAT(a.BirthDate, 'MM-dd-yyyy'),
+	     c.[Name] AS NomeTown,
+	     a.Email
+    FROM Accounts AS a
+    JOIN Cities AS c
+      ON a.CityId = c.Id
+   WHERE Email LIKE 'e%'
+ORDER BY c.[Name]

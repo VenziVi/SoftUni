78fdce97daddb9e3541846288a674d842b2c55b4
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-14 07:50:30 +0200

    export projects with tasks

--- Entity-Framework-Core/Exams/Exam-04.04.2021/TeisterMask/DataProcessor/Serializer.cs
+++ Entity-Framework-Core/Exams/Exam-04.04.2021/TeisterMask/DataProcessor/Serializer.cs
@@ -16,7 +16,39 @@
     {
         public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
         {
+	    var sb = new StringBuilder();
 
+            using StringWriter writer = new StringWriter(sb);
+
+            XmlSerializer serializer = new XmlSerializer(typeof(ProjectsExportDto[]), new XmlRootAttribute("Projects"));
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+       
+            var projects = context
+                .Projects
+                .ToArray()
+                .Where(p => p.Tasks.Any())
+                .Select(p => new ProjectsExportDto
+                {
+                    TasksCount = p.Tasks.Count,
+                    ProjectName = p.Name,
+                    HasEndDate = p.DueDate.HasValue ? "Yes" : "No",
+                    Tasks = p.Tasks.Select(t => new TaskDto
+                    {
+                        Name = t.Name,
+                        Label = t.LabelType.ToString()
+                    })
+                    .OrderBy(t => t.Name)
+                    .ToArray()
+                })
+                .OrderByDescending(p => p.TasksCount)
+                .ThenBy(p => p.ProjectName)
+                .ToArray();
+
+            serializer.Serialize(writer, projects, namespaces);
+
+            return sb.ToString().TrimEnd();	
         }
 
         public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)

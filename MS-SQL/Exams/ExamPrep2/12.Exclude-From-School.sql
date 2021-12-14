386b71b9c72acd31d952d4db49229cd8856ef7f8
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-14 07:47:09 +0200

    exclude from school

---
+++ MS-SQL/Exams/ExamPrep2/12.Exclude-From-School.sql
@@ -0,0 +1,18 @@
+CREATE PROC usp_ExcludeFromSchool(@StudentId INT)
+AS
+BEGIN
+	IF(NOT EXISTS (SELECT 1 FROM Students WHERE Id = @StudentId))
+		 RAISERROR('This school has no student with the provided id!',16,1)
+
+	DELETE FROM StudentsExams
+		WHERE StudentId = @StudentId
+
+	DELETE FROM StudentsSubjects
+		WHERE StudentId = @StudentId
+
+	DELETE FROM StudentsTeachers
+		WHERE StudentId = @StudentId
+
+	DELETE FROM Students
+		WHERE Id = @StudentId
+END

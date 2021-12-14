fb84f2ce893e9d60f645779292940c1391bd8300
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-14 07:46:26 +0200

    exam grades query

---
+++ MS-SQL/Exams/ExamPrep2/11.Exam-Grades.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION udf_ExamGradesToUpdate(@studentId INT, @grade DECIMAL(18,2))
+RETURNS VARCHAR(MAX) AS 
+BEGIN
+	IF(@grade > 6.00)
+		RETURN 'Grade cannot be above 6.00!'
+	IF(NOT EXISTS (SELECT 1 FROM Students WHERE Id = @studentId))
+		RETURN 'The student with provided id does not exist in the school!'
+
+	DECLARE @GradesCount INT = (SELECT COUNT(*) FROM StudentsExams WHERE StudentId = @studentId AND Grade BETWEEN @grade AND (@grade + 0.50))
+
+	DECLARE @FirstName NVARCHAR(30) = (SELECT FirstName FROM Students WHERE Id = @studentId)
+
+	RETURN CONCAT('You have to update ', @GradesCount, ' grades for the student ', @FirstName)
+END

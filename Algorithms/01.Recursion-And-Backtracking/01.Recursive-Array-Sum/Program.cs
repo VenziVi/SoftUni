7d8cffb72b41355b41f358a6039b8187fb92d2d6
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-16 07:46:26 +0200

    add recursive array sum

---
+++ Algorithms/01.Recursion-And-Backtracking/01.Recursive-Array-Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+
+Console.WriteLine(RecursiveSum(arr, 0));
+
+int RecursiveSum(int[] array, int i)
+{
+    if (i == array.Length - 1)
+    {
+        return array[i];
+    }
+
+    return array[i] + RecursiveSum(array, i + 1);
+}

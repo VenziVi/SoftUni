ff26e260c186a8603d07eba4d288420a9ad45513
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-16 07:48:03 +0200

    add recursive drawing

---
+++ Algorithms/01.Recursion-And-Backtracking/2.Recursive-Drawing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2.RecursiveDrawing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            DrawRecursivly(n);
+        }
+
+        private static void DrawRecursivly(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new String('*', n));
+            DrawRecursivly(n - 1);
+            Console.WriteLine(new String('#', n));
+        }
+    }
+}

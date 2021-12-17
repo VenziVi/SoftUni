e31e9389868e25140c410e0ae39794cab6732c29
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-17 07:44:58 +0200

    add recursive factorial

---
+++ Algorithms/01.Recursion-And-Backtracking/04.RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04.RecursiveFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FacturialCalc(n));
+        }
+
+        private static int FacturialCalc(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * FacturialCalc(n - 1);
+        }
+    }
+}

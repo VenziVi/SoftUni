e789e3b7d131b8c9fc926676c59fcc0ed7a750eb
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-19 18:55:25 +0200

    added recursive fibonacci

---
+++ Algorithms/01.Recursion-And-Backtracking/07.RecursiveFibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.RecursiveFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(CalcFib(n));
+        }
+
+        static long CalcFib(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+
+            return CalcFib(number - 1) + CalcFib(number - 2);
+        }
+    }
+}

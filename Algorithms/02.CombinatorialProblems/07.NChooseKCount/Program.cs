8eaf11a415d38a00a50fa106f58e2bde69c8f3ee
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-24 21:13:09 +0200

    n choose k count

---
+++ Algorithms/02.CombinatorialProblems/07.NChooseKCount/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.NChooseKCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Binom(n, k));
+        }
+
+        static long Binom(int n, int k)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            if (k == 0 || k == n)
+            {
+                return 1;
+            }
+
+            return Binom(n - 1, k) + Binom(n - 1, k - 1);
+        }
+    }
+}

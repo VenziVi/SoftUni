a6f9dfb738709b249b1532b2bf38ca6184a8dbeb
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-26 10:54:24 +0200

    added export theatres

--- Entity-Framework-Core/Exams/Exam-04.12.2021/Theatre/DataProcessor/Serializer.cs
+++ Entity-Framework-Core/Exams/Exam-04.12.2021/Theatre/DataProcessor/Serializer.cs
@@ -14,7 +14,32 @@
     {
         public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
         {
+		var theatres = context
+                .Theatres
+                .ToArray()
+                .Where(t => t.NumberOfHalls >= numbersOfHalls && t.Tickets.Count() >= 20)
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Halls = t.NumberOfHalls,
+                    TotalIncome = decimal.Parse(t.Tickets
+                    .Where(x => x.RowNumber >= 1 && x.RowNumber <= 5)
+                    .Sum(t => t.Price).ToString("f2")),
+                    Tickets = t.Tickets.Where(x => x.RowNumber >= 1 && x.RowNumber <= 5).Select(c => new
+                    {
+                        Price = decimal.Parse(c.Price.ToString("f2")),
+                        RowNumber = c.RowNumber
+                    })
+                    .OrderByDescending(c => c.Price)
+                    .ToArray()
+                })
+                .OrderByDescending(t => t.Halls)
+                .ThenBy(t => t.Name)
+                .ToArray();
 
+            var result = JsonConvert.SerializeObject(theatres, Formatting.Indented);
+
+            return result.ToString().TrimEnd();
         }
 
         public static string ExportPlays(TheatreContext context, double rating)

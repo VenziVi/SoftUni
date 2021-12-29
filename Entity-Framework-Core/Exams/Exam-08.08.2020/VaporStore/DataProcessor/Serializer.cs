62ec02943977682ec574f35d9f5e65b6f2be6ac2
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-29 07:46:56 +0200

    export games by genres

--- Entity-Framework-Core/Exams/Exam-08.08.2020/VaporStore/DataProcessor/Serializer.cs
+++ Entity-Framework-Core/Exams/Exam-08.08.2020/VaporStore/DataProcessor/Serializer.cs
@@ -15,7 +15,35 @@
 	{
 		public static string ExportGamesByGenres(VaporStoreDbContext context, string[] genreNames)
 		{
-			
+			var games = context
+				.Genres
+				.ToArray()
+				.Where(g => genreNames.Contains(g.Name))
+				.Select(g => new
+				{
+					Id = g.Id,
+					Genre = g.Name,
+					Games = g.Games.Select(d => new
+					{
+						Id = d.Id,
+						Title = d.Name,
+						Developer = d.Developer.Name,
+						Tags = string.Join(", ", d.GameTags.Select(x => x.Tag.Name)),
+						Players = d.Purchases.Count
+					})
+					.Where(g => g.Players > 0)
+					.OrderByDescending(d => d.Players)
+					.ThenBy(d => d.Id)
+					.ToArray(),
+					TotalPlayers = g.Games.Sum(g => g.Purchases.Count())
+				})
+				.OrderByDescending(g => g.TotalPlayers)
+				.ThenBy(g => g.Id)
+				.ToArray();
+
+			var result = JsonConvert.SerializeObject(games, Formatting.Indented);
+
+			return result;
 		}
 
 		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)

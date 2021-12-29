353ae12acd66a65b0a067cdd079f3df8529e6a07
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-29 07:47:47 +0200

    export users purchases

--- Entity-Framework-Core/Exams/Exam-08.08.2020/VaporStore/DataProcessor/Serializer.cs
+++ Entity-Framework-Core/Exams/Exam-08.08.2020/VaporStore/DataProcessor/Serializer.cs
@@ -48,7 +48,49 @@
 
 		public static string ExportUserPurchasesByType(VaporStoreDbContext context, string storeType)
 		{
-			
+			var users = context
+				.Users
+				.ToArray()
+				.Select(u => new UsersExport
+				{
+					Username = u.Username,
+					TotalSpent = u.Cards.SelectMany(c => c.Purchases.Where(p => p.Type == Enum.Parse<PurchaseType>(storeType)))
+					.Sum(p => p.Game.Price),
+					Purchases = u.Cards
+					.SelectMany(p => p.Purchases.Where(p => p.Type == Enum.Parse<PurchaseType>(storeType)))
+					.OrderBy(p => p.Date)
+					.Select(p => new PurchasesExport
+					{
+						Card = p.Card.Number,
+						Cvc = p.Card.Cvc,
+						Date = p.Date.ToString("yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture),
+						Game = new GameExport
+						{
+							Title = p.Game.Name,
+							Genre = p.Game.Genre.Name,
+							Price = p.Game.Price
+						}
+					})
+					.ToArray()
+				})
+				.OrderByDescending(u => u.TotalSpent)
+				.ThenBy(u => u.Username)
+				.ToArray();
+
+			users = users.Where(u => u.Purchases.Count() > 0).ToArray();
+
+			var sb = new StringBuilder();
+			using StringWriter writer = new StringWriter(sb);
+
+			XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+
+			namespaces.Add(string.Empty, string.Empty);
+
+			XmlSerializer seriliazer = new XmlSerializer(typeof(UsersExport[]), new XmlRootAttribute("Users"));
+
+			seriliazer.Serialize(writer, users, namespaces);
+
+			return sb.ToString().TrimEnd();
 		}
 	}
 }

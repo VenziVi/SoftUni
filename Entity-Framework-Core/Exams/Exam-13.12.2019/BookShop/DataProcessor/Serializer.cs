31227d8cae541353e09abb4786c310a117bfb186
Ventsislav Lazarov <vlazarov86@gmail.com>
2021-12-30 07:47:33 +0200

    book store

---
+++ Entity-Framework-Core/Exams/Exam-13.12.2019/BookShop/DataProcessor/Serializer.cs
@@ -0,0 +1,28 @@
+﻿namespace BookShop.DataProcessor
+{
+    using BookShop.Data.Models.Enums;
+    using BookShop.DataProcessor.ExportDto;
+    using Data;
+    using Newtonsoft.Json;
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Formatting = Newtonsoft.Json.Formatting;
+
+    public class Serializer
+    {
+        public static string ExportMostCraziestAuthors(BookShopContext context)
+        {
+            
+           
+        }
+
+        public static string ExportOldestBooks(BookShopContext context, DateTime date)
+        {
+           
+        }
+    }
+}

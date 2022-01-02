ab560a834c5bf437d5d00ba58e42d1365a6c4606
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-02 11:36:55 +0200

    added softjail

---
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/Data/Models/OfficerPrisoner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SoftJail.Data.Models
+{
+    public class OfficerPrisoner
+    {
+        [ForeignKey(nameof(Prisoner))]
+        public int PrisonerId { get; set; }
+        public virtual Prisoner Prisoner { get; set; }
+
+        [ForeignKey(nameof(Officer))]
+        public int OfficerId { get; set; }
+        public virtual Officer Officer { get; set; }
+    }
+}
+//•	PrisonerId – integer, Primary Key
+//•	Prisoner – the officer’s prisoner (required)
+//•	OfficerId – integer, Primary Key
+//•	Officer – the prisoner’s officer (required)
---
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/Serializer.cs
@@ -0,0 +1,33 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+            
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}

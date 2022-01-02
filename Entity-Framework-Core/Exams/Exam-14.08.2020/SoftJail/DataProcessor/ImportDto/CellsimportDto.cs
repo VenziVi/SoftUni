cb67361215ddbd258ecd68f44eaae7c4cf7d46e1
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-02 11:39:40 +0200

    added cells import dto

---
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/ImportDto/CellsimportDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class CellsimportDto
+    {
+        [Range(1, 1000)]
+        public int CellNumber { get; set; }
+
+        [Required]
+        public string HasWindow { get; set; }
+    }
+}
+//•	CellNumber – integer in the range [1, 1000] (required)
+//•	HasWindow – bool(required)

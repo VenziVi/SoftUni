b39d6bbda296a1b3783b7246a366b25186d990ad
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-03 12:19:00 +0200

    added departments import dto

---
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/ImportDto/DepartmentsImportDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class DepartmentsImportDto
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public CellsimportDto[] Cells { get; set; }
+    }
+}
+//•	Name – text with min length 3 and max length 25 (required)

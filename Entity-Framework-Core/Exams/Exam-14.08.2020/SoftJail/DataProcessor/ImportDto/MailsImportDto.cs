21b31e6edd98280d6d501b886e5303bbb5c415f9
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-03 12:19:27 +0200

    added mails import dto

---
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/ImportDto/MailsImportDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class MailsImportDto
+    {
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Sender { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-z0-9\s]+(str.)$")]
+        public string Address { get; set; }
+    }
+}
+//•	Description– text (required)
+//•	Sender – text(required)
+//•	Address – text, consisting only of letters, spaces and numbers, which ends with “ str.” (required) (Example: “62 Muir Hill str.“)

ff50fc3c96114f854d74b6095cece647db313957
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-03 12:19:50 +0200

    added officer import dto

---
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/ImportDto/OfficerImportDto.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class OfficerImportDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [StringLength(30, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [XmlElement("Money")]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Money { get; set; }
+
+        [XmlElement("Position")]
+        [Required]
+        public string Position { get; set; }
+
+        [XmlElement("Weapon")]
+        [Required]
+        public string Weapon { get; set; }
+
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public OfficerPrisonersImportDto[] Prisoners { get; set; }
+
+    }
+}
+//•	FullName – text with min length 3 and max length 30 (required)
+//•	Salary – decimal(non - negative, minimum value: 0)(required)
+//•	Position - Position enumeration with possible values: “Overseer, Guard, Watcher, Labour” (required)
+//•	Weapon - Weapon enumeration with possible values: “Knife, FlashPulse, ChainRifle, Pistol, Sniper” (required)
+//•	DepartmentId - integer, foreign key(required)
+//•	Department – the officer's department (required)

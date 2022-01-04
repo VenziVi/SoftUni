ada9a59b3ce801149fdad75f82d706280975de46
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-04 07:51:12 +0200

    added officer prisoners dto

---
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/ImportDto/OfficerPrisonersImportDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Prisoner")]
+    public class OfficerPrisonersImportDto
+    {
+        [XmlAttribute("id")]
+        public int id { get; set; }
+    }
+}

0104a8d495d1475cb1b0e932b7630207bbdc04e4
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-04 07:51:52 +0200

    added prisoners import dto

---
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/ImportDto/PrisonersImportDto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class PrisonersImportDto
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string FullName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^The\s[A-Z][a-z]+$")]
+        public string Nickname { get; set; }
+
+        [Range(18, 65)]
+        public int Age { get; set; }
+
+        [Required]
+        public string IncarcerationDate { get; set; }
+
+        public string ReleaseDate { get; set; }
+
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal? Bail { get; set; }
+
+        public int? CellId { get; set; }
+
+        public MailsImportDto[] Mails { get; set; }
+    }
+}
+//•	FullName – text with min length 3 and max length 20 (required)
+//•	Nickname – text starting with "The " and a single word only of letters with an uppercase letter for beginning(example: The Prisoner) (required)
+//•	Age – integer in the range[18, 65] (required)
+//•	IncarcerationDate ¬– Date(required)
+//•	ReleaseDate– Date
+//•	Bail– decimal(non - negative, minimum value: 0)
+//•	CellId - integer, foreign key
+//•	Cell – the prisoner's cell

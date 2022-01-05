8762ef776d9396a7760d3585071e392270b196d3
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-05 07:50:20 +0200

    added nested loops problem

---
+++ Algorithms/03.RecursionAndCombinatorialProblems-Exercise/02.NestedLoops/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.NestedLoops
+{
+    internal class Program
+    {
+        private static int[] result;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            result = new int[n];
+
+            Variation(0);
+        }
+
+        private static void Variation(int index)
+        {
+            if (index >= result.Length)
+            {
+                Console.WriteLine(string.Join(" ", result));
+                return;
+            }
+
+            for (int i = 1; i <= result.Length; i++)
+            {
+                result[index] = i;
+                Variation(index + 1);
+            }
+        }
+    }
+}

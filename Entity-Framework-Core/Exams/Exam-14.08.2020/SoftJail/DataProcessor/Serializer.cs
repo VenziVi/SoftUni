bcb368614c3c4e55da7d95b350f4164103fef26e
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-07 07:50:40 +0200

    export prisoners by cells

--- Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/Serializer.cs
+++ Entity-Framework-Core/Exams/Exam-14.08.2020/SoftJail/DataProcessor/Serializer.cs
@@ -15,7 +15,29 @@
     {
         public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
         {
-            
+            var prisoners = context
+                .Prisoners
+                .Where(p => ids.Contains(p.Id))
+                .ToArray()
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.FullName,
+                    CellNumber = p.Cell.CellNumber,
+                    Officers = p.PrisonerOfficers.Select(o => new
+                    {
+                        OfficerName = o.Officer.FullName,
+                        Department = o.Officer.Department.Name,
+                    })
+                    .OrderBy(o => o.OfficerName),
+                    TotalOfficerSalary = decimal.Parse(p.PrisonerOfficers.Select(po => po.Officer).Sum(po => po.Salary).ToString("f2"))
+                })
+                .OrderBy(p => p.Name)
+                .ThenBy(p => p.Id)
+                .ToArray();
+
+            var result = JsonConvert.SerializeObject(prisoners, Formatting.Indented);
+            return result.ToString();
         }
 
         public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)

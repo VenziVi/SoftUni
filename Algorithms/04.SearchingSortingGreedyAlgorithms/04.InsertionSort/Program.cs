974d5dcba3a1546405364f580e45f91d75cdd555
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-11 22:19:39 +0200

    added insertion sort

---
+++ Algorithms/04.SearchingSortingGreedyAlgorithms/04.InsertionSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            var sortedArr = Insertion(input);
+            Console.WriteLine(string.Join(" ", sortedArr));
+        }
+
+        private static int[] Insertion(int[] input)
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                var j = i;
+
+                while (j > 0 && input[j] < input[j - 1])
+                {
+                    Swap(input, j, j - 1);
+                    j--;
+                }
+            }
+
+            return input;
+        }
+
+        private static void Swap(int[] input, int index, int min)
+        {
+            var temp = input[index];
+            input[index] = input[min];
+            input[min] = temp;
+        }
+    }
+}

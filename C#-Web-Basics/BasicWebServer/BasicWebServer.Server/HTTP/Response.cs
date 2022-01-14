e426ed6da1f57e1c0bf07d11d2c93feb7b1ea06e
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-14 22:06:48 +0200

    added request class

---
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/Response.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicWebServer.Server.HTTP
+{
+    public class Response
+    {
+        public Response(StatusCode _statusCode)
+        {
+            StatusCode = _statusCode;
+
+            Header.Add("Server", "My Web Server");
+            Header.Add("Date", $"{DateTime.UtcNow:R}");
+        }
+
+        public StatusCode StatusCode { get; init; }
+        public HeaderCollection Header { get; } = new HeaderCollection();
+
+        public string Body { get; set; }
+
+    }
+}

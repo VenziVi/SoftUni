46d4208b7cc21c4c9b38ab2c5651fae641f3fbf6
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-14 21:38:55 +0200

    added header collection

---
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class Header
+    {
+        public Header(string _name, string _value)
+        {
+            Name = _name;
+            Value = _value;
+        }
+
+        public string Name { get; init; }
+        public string Value { get; set; }
+    }
+}
---
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/HeaderCollection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.HTTP
+{
+    public class HeaderCollection
+    {
+        private readonly Dictionary<string, Header> headers;
+
+        public HeaderCollection() => headers = new Dictionary<string, Header>();
+
+        public int Count => headers.Count;
+
+        public void Add(string name, string Value)
+        {
+            var header = new Header(name, Value);
+            headers.Add(name, header);
+        }
+    }
+}

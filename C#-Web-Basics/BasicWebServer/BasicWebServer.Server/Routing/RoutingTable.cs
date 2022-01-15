8bd37b641a9bcc25c498c8b8c49e16eaf2f5374d
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-15 12:12:25 +0200

    added get requests

---
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/Routing/RoutingTable.cs
@@ -0,0 +1,60 @@
+﻿using BasicWebServer.Server.Common;
+using BasicWebServer.Server.HTTP;
+using BasicWebServer.Server.Responses;
+
+namespace BasicWebServer.Server.Routing
+{
+    public class RoutingTable : IRoutingTable
+    {
+        private readonly Dictionary<Method, Dictionary<string, Response>> routes;
+
+        public RoutingTable() =>
+            routes = new ()
+        {
+                [Method.GET] = new (),
+                [Method.PUT] = new (),
+                [Method.PUT] = new (),
+                [Method.DELETE] = new ()
+        };
+
+        public IRoutingTable Map(string url, Method method, Response response)
+            => method switch
+            {
+                Method.GET => this.MapGet(url, response),
+                Method.POST => this.MapPost(url, response),
+                _ => throw new InvalidOperationException($"{method} is not supported.");
+        };
+
+        public IRoutingTable MapGet(string url, Response response)
+        {
+            Guard.AgainstNull(url, nameof(url));
+            Guard.AgainstNull(response, nameof(response));
+
+            routes[Method.GET][url] = response;
+            return this;
+        }
+
+        public IRoutingTable MapPost(string url, Response response)
+        {
+            Guard.AgainstNull(url, nameof(url));
+            Guard.AgainstNull(response, nameof(response));
+
+            routes[Method.POST][url] = response;
+            return this;
+        }
+
+        public Response MatchRequest(Request request)
+        {
+            var requestMethod = request.Method;
+            var requestUrl = request.Url;
+
+            if (!routes.ContainsKey(requestMethod) ||
+                routes[requestMethod].ContainsKey(requestUrl))
+            {
+                return new NotFoundResponse();
+            }
+
+            return routes[requestMethod][requestUrl];
+        }
+    }
+}

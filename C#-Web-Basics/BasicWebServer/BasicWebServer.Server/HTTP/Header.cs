260bfe301416ecbc0e5b84d06860265d59678225
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-15 10:57:03 +0200

    added guard class

--- C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
@@ -1,9 +1,14 @@
-﻿namespace BasicWebServer.Server.HTTP
+﻿using BasicWebServer.Server.Common;
+
+namespace BasicWebServer.Server.HTTP
 {
     public class Header
     {
         public Header(string _name, string _value)
         {
+            Guard.AgainstNull(_name, nameof(_name));
+            Guard.AgainstNull(_value, nameof(_value));
+
             Name = _name;
             Value = _value;
         }

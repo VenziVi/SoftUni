8af23d17da3a432c11e5972877f2bd3beeb1ecdc
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-17 21:33:16 +0200

    added file response for download web content

---
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/TextFileResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace BasicWebServer.Server.HTTP
+{
+    internal class TextFileResponse : Response
+    {
+        public TextFileResponse(string _fileName) 
+            : base(StatusCode.OK)
+        {
+            FileName = _fileName;
+
+            Headers.Add(Header.ContentType, ContentType.PlainText);
+        }
+
+        public string FileName { get; init; }
+
+        public override string ToString()
+        {
+            if (File.Exists(FileName))
+            {
+                Body = File.ReadAllTextAsync(FileName).Result;
+
+                var fileBytesCount = new FileInfo(FileName).Length;
+                Headers.Add(Header.ContentLength, fileBytesCount.ToString());
+
+                Headers.Add(Header.ContentDisposition,
+                    $"attachment; filename=\"{FileName}\"");
+            }
+
+            return base.ToString();
+        }
+    }
+}

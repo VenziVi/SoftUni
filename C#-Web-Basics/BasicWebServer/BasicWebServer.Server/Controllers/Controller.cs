6048fc49d2a5f101b6347e962647fe99c0564fbe
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-01-26 22:18:27 +0200

    implement controller base class

--- C#-Web-Basics/BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/Controllers/Controller.cs
@@ -1,19 +1,29 @@
 ﻿using BasicWebServer.Server.HTTP;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BasicWebServer.Server.Responses;
 
 namespace BasicWebServer.Server.Controllers
 {
-    public class Controller
+    public abstract class Controller
     {
         public Controller(Request request)
         {
             Request = request;
         }
 
-        public Request Request { get; }
+        protected Request Request { get; private init; }
+
+        protected Response Text(string text) => new TextResponse(text);
+
+        protected Response Html(string text) => new HtmlResponse(text);
+
+        protected Response BadRequest() => new BadRequestResponse();
+
+        protected Response Unauthorized() => new UnauthorizedResponse();
+
+        protected Response NotFoind() => new NotFoundResponse();
+
+        protected Response Redirect(string location) => new RedirectResponse(location);
+
+        protected Response File(string fileName) => new TextFileResponse(fileName);
     }
 }

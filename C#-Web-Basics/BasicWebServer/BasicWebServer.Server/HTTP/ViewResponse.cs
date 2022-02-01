b8ee2c0d8c958f46222eae95deb011996b26aa0e
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-01 22:45:35 +0200

    implement view response

---
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/ViewResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace BasicWebServer.Server.HTTP
+{
+    public class ViewResponse : ContentResponse
+    {
+        private const char PathSeparator = '/';
+        public ViewResponse(string _viewName, string _controllerName) 
+            : base("", ContentType.Html)
+        {
+            if (!_viewName.Contains(PathSeparator))
+            {
+                _viewName = _controllerName + PathSeparator + _viewName;
+            }
+
+            var viewPath = Path.GetFullPath($"./Views/" + _viewName.TrimStart(PathSeparator) + ".cshtml");
+            var viewContent = File.ReadAllText(viewPath);
+
+            this.Body = viewContent;
+        }
+    }
+}

e0bf34aeb50d4be42a8228508d2d8ae1a0269415
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-02 20:56:06 +0200

    added PopulateModel method to ViewResponse

--- C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/ViewResponse.cs
+++ C#-Web-Basics/BasicWebServer/BasicWebServer.Server/HTTP/ViewResponse.cs
@@ -1,9 +1,11 @@
-﻿namespace BasicWebServer.Server.HTTP
+﻿
+
+namespace BasicWebServer.Server.HTTP
 {
     public class ViewResponse : ContentResponse
     {
         private const char PathSeparator = '/';
-        public ViewResponse(string _viewName, string _controllerName) 
+        public ViewResponse(string _viewName, string _controllerName, object model = null) 
             : base("", ContentType.Html)
         {
             if (!_viewName.Contains(PathSeparator))
@@ -14,7 +16,36 @@
             var viewPath = Path.GetFullPath($"./Views/" + _viewName.TrimStart(PathSeparator) + ".cshtml");
             var viewContent = File.ReadAllText(viewPath);
 
+            if (model != null)
+            {
+                viewContent = PopulateModel(viewContent, model);
+            }
+
             this.Body = viewContent;
+        }
+
+        private string PopulateModel(string viewContent, object model)
+        {
+            var data = model
+                .GetType()
+                .GetProperties()
+                .Select(p => new
+                {
+                    p.Name,
+                    Value = p.GetValue(model)
+                });
+
+            foreach (var item in data)
+            {
+                const string opening = "{{";
+                const string closing = "}}";
+
+                viewContent = viewContent.Replace(
+                    $"{opening}{item.Name}{closing}",
+                    item.Value.ToString());    
+            }
+
+            return viewContent;
         }
     }
 }

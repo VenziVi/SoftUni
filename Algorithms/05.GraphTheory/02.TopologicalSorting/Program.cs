f972be35256a5e8ab44ac73a492ffd90072a343d
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-09 10:18:38 +0200

    added topological sorting

---
+++ Algorithms/05.GraphTheory/02.TopologicalSorting/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.TopologicalSorting
+{
+    internal class Program
+    {
+        private static Dictionary<string, List<string>> graph;
+        private static Dictionary<string, int> dependencies;
+
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            graph = ReadGraph(n);
+
+            dependencies = ExtractDependencies(graph);
+
+            var sorted = new List<string>();
+
+            while (dependencies.Count > 0)
+            {
+                var nodeToRemove = dependencies.FirstOrDefault(d => d.Value == 0).Key;
+
+                if (nodeToRemove == null)
+                {
+                    break;
+                }
+
+                dependencies.Remove(nodeToRemove);
+                sorted.Add(nodeToRemove);
+
+                foreach (var child in graph[nodeToRemove])
+                {
+                    dependencies[child]--;
+                }
+            }
+
+            if (dependencies.Count == 0)
+            {
+                Console.WriteLine($"Topological sorting: {string.Join(", ", sorted)}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid topological sorting");
+            }
+        }
+
+        private static Dictionary<string, int> ExtractDependencies(Dictionary<string, List<string>> graph)
+        {
+            var result = new Dictionary<string, int>();
+
+            foreach (var kvp in graph)
+            {
+                var node = kvp.Key;
+                var children = kvp.Value;
+
+                if (!result.ContainsKey(node))
+                {
+                    result[node] = 0;
+                }
+
+                foreach (var child in children)
+                {
+                    if (!result.ContainsKey(child))
+                    {
+                        result[child] = 1;
+                    }
+                    else
+                    {
+                        result[child]++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private static Dictionary<string, List<string>> ReadGraph(int n)
+        {
+            var result = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine()
+                    .Split("->", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => x.Trim())
+                    .ToArray();
+
+                if (line.Length > 1)
+                {
+                    var edges = line[1].Split(", ").ToList();
+                    result[line[0]] = edges;
+                }
+                else
+                {
+                    result[line[0]] = new List<string>();
+                }
+            }
+
+            return result;
+        }
+    }
+}

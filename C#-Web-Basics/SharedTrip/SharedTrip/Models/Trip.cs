6c4ba34b93ca1438307596fe0a5079127109128a
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-11 19:51:32 +0200

    implement trip model

--- C#-Web-Basics/SharedTrip/SharedTrip/Models/Trip.cs
+++ C#-Web-Basics/SharedTrip/SharedTrip/Models/Trip.cs
@@ -1,7 +1,38 @@
-﻿namespace SharedTrip.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.Models
 {
     public class Trip
     {
+        public Trip()
+        {
+            UserTrips = new HashSet<UserTrip>();
+            Id = Guid.NewGuid().ToString();
+        }
 
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string StartPoint { get; set; }
+
+        [Required]
+        public string EndPoint { get; set; }
+
+        [Required]
+        public DateTime DepartureTime { get; set; }
+
+        [Range(2, 6)]
+        public int Seats { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public ICollection<UserTrip> UserTrips { get; set; }
     }
 }

1e1aca518031000d53f2250df6ad99dbd60a994d
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-11 19:51:46 +0200

    implement user model

--- C#-Web-Basics/SharedTrip/SharedTrip/Models/User.cs
+++ C#-Web-Basics/SharedTrip/SharedTrip/Models/User.cs
@@ -1,7 +1,31 @@
-﻿namespace SharedTrip.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SharedTrip.Models
 {
     public class User
     {
-        
+        public User()
+        {
+            UserTrips = new HashSet<UserTrip>();
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        public ICollection<UserTrip> UserTrips { get; set; }
     }
 }

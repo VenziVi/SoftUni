2b7bda7cd367dcc554bb77735015df0f363aa7fe
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-11 19:51:59 +0200

    implement userTrip model

--- C#-Web-Basics/SharedTrip/SharedTrip/Models/UserTrip.cs
+++ C#-Web-Basics/SharedTrip/SharedTrip/Models/UserTrip.cs
@@ -1,7 +1,17 @@
-﻿namespace SharedTrip.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharedTrip.Models
 {
     public class UserTrip
     {
-        
+        [ForeignKey(nameof(User))]
+        public string UserId { get; set; }
+
+        public User User { get; set; }
+
+        [ForeignKey(nameof(Trip))]
+        public string TripId { get; set; }
+
+        public Trip Trip { get; set; }
     }
 }

015f7f1c9ee4f854d79560051c4bda19bd7bea32
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-12 16:54:23 +0200

    implement cart model

--- C#-Web-Basics/SMS/SMS/Models/Cart.cs
+++ C#-Web-Basics/SMS/SMS/Models/Cart.cs
@@ -1,6 +1,24 @@
-﻿namespace SMS.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMS.Models
 {
     public class Cart
     {
+        public Cart()
+        {
+            Products = new HashSet<Product>();
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }

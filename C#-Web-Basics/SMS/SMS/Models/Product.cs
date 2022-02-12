0dd44e339be4dc2b6c7602bc10ccd99ed014b194
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-12 16:54:36 +0200

    implement product model

--- C#-Web-Basics/SMS/SMS/Models/Product.cs
+++ C#-Web-Basics/SMS/SMS/Models/Product.cs
@@ -1,6 +1,29 @@
-﻿namespace SMS.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMS.Models
 {
     public class Product
     {
+        public Product()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Name { get; set; }
+
+        [Range(0.005, 1000)]
+        public decimal Price { get; set; }
+
+        [ForeignKey(nameof(Cart))]
+        public string CartId { get; set; }
+
+        public Cart Cart { get; set; }
     }
 }

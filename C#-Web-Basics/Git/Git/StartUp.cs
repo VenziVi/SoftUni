f66c5d91caa4f22e31cdbf777c5fefdfe6d3c501
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-15 08:01:45 +0200

    added dependencies

---
+++ C#-Web-Basics/Git/Git/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace Git
+{
+    using BasicWebServer.Server;
+    using BasicWebServer.Server.Routing;
+    using Git.Contracts;
+    using Git.Data;
+    using Git.Data.Common;
+    using Git.Services;
+    using System.Threading.Tasks;
+
+    public class Startup
+    {
+        public static async Task Main()
+        {
+            var server = new HttpServer(routes => routes
+               .MapControllers()
+               .MapStaticFiles());
+            
+            server.ServiceCollection
+                .Add<ApplicationDbContext>()
+                .Add<IDbRepository, DbRepository>()
+                .Add<IValidationService, ValidationService>()
+                .Add<IUserService, UserService>()
+                .Add<IRepositoryService, RepositoryService>()
+                .Add<ICommitService, CommitService>();
+            
+            await server.Start();
+        }
+    }
+}

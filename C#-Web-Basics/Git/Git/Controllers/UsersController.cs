f8a3e82b4cc3fa648708a746ad16ebfbe4e8d5e6
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-15 08:02:42 +0200

    implement users controler

---
+++ C#-Web-Basics/Git/Git/Controllers/UsersController.cs
@@ -0,0 +1,84 @@
+﻿using BasicWebServer.Server.Attributes;
+using BasicWebServer.Server.Controllers;
+using BasicWebServer.Server.HTTP;
+using Git.Contracts;
+using Git.Data.ViewModels;
+
+namespace Git.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly IUserService userService;
+        public UsersController(Request request,
+            IUserService _userService) 
+            : base(request)
+        {
+            userService = _userService;
+        }
+
+        public Response Register()
+        {
+            if (User.IsAuthenticated)
+            {
+                return Redirect("/Repositories/All");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public Response Register(RegisterViewModel model)
+        {
+            bool isRegistred = userService.IsRegistred(model);
+
+            if (isRegistred)
+            {
+                return Redirect("/Users/Login");
+            }
+
+            return Redirect("/Users/Register");
+        }
+
+        public Response Login()
+        {
+            if (User.IsAuthenticated)
+            {
+                return Redirect("/Repositories/All");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public Response Login(LoginViewModel model)
+        {
+            if (User.IsAuthenticated)
+            {
+                return Redirect("/Repositories/All");
+            }
+
+            string id = userService.IsLogged(model);
+
+            if (id == null)
+            {
+                return Redirect("/Users/Login");
+            }
+
+            SignIn(id);
+
+            CookieCollection cookies = new CookieCollection();
+            cookies.Add(Session.SessionCookieName,
+                Request.Session.Id);
+
+            return Redirect("/Repositories/All");
+        }
+
+        [Authorize]
+        public Response Logout()
+        {
+            SignOut();
+
+            return Redirect("/");
+        }
+    }
+}
---
+++ C#-Web-Basics/Git/Git/Services/UserService.cs
@@ -0,0 +1,87 @@
+﻿using Git.Contracts;
+using Git.Data.Common;
+using Git.Data.Models;
+using Git.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IDbRepository repo;
+
+        private readonly IValidationService validationService;
+
+        public UserService(
+            IDbRepository _repo,
+            IValidationService _validationService
+            )
+        {
+            repo = _repo;
+            validationService = _validationService;
+        }
+
+        public string IsLogged(LoginViewModel model)
+        {
+            var user = repo.All<User>()
+                .Where(u => u.Username == model.Username)
+                .Where(u => u.Password == GetHash(model.Password))
+                .FirstOrDefault();
+
+            return user.Id;
+        }
+
+        public bool IsRegistred(RegisterViewModel model)
+        {
+            bool isRegistred = false;
+
+            var isValid = validationService.ValidateModel(model);
+
+            if (string.IsNullOrWhiteSpace(model.Username) ||
+                string.IsNullOrWhiteSpace(model.Email) ||
+                string.IsNullOrWhiteSpace(model.Password))
+            {
+                isValid = false;
+            }
+
+            if (!isValid)
+            {
+                return isRegistred;
+            }
+
+            User user = new User()
+            {
+                Username = model.Username,
+                Email = model.Email,
+                Password = GetHash(model.Password)
+            };
+
+            try
+            {
+                repo.Add(user);
+                repo.SaveChanges();
+                isRegistred = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            return isRegistred;
+        }
+
+        private string GetHash(string password)
+        {
+            byte[] passworArray = Encoding.UTF8.GetBytes(password);
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return Convert.ToBase64String(sha256.ComputeHash(passworArray));
+            }
+        }
+    }
+}

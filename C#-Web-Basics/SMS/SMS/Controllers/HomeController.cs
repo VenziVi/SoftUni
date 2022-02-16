8edf66f9ba00f683535ae3f4965460d25d21227a
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-16 23:37:58 +0200

    implement home service

--- C#-Web-Basics/SMS/SMS/Controllers/HomeController.cs
+++ C#-Web-Basics/SMS/SMS/Controllers/HomeController.cs
@@ -1,25 +1,43 @@
 ﻿using BasicWebServer.Server.Controllers;
 using BasicWebServer.Server.HTTP;
+using SMS.Contracts;
+using SMS.Models.HomeModels;
+using System.Collections.Generic;
 
 namespace SMS.Controllers
 {
     public class HomeController : Controller
     {
-        public HomeController(Request request) 
+        private readonly IHomeService homeService;
+        private readonly IUserService userService;
+
+        public HomeController(Request request,
+            IHomeService _homeService,
+            IUserService _userService) 
             : base(request)
         {
+            homeService = _homeService;
+            userService = _userService;
         }
 
         public Response Index()
         {
-            var model = new 
+            if (User.IsAuthenticated)
             {
-                IsAuthenticated = User.IsAuthenticated,
-            };
+                IEnumerable<AllProductsViewModel> products = homeService.GetProducts();
+                var username = userService.GetUserName(User.Id);
 
-            return this.View(model);
+                var model = new
+                {
+                    username = username,
+                    products = products,
+                    IsAuthenticated = true
+                };
+
+                return View(model, "/Home/IndexLoggedin");
+            }
+
+            return View(new { IsAuthenticated = false});
         }
-
-        public Response IndexLoggedin() => View();
     }
 }
---
+++ C#-Web-Basics/SMS/SMS/Services/HomeService.cs
@@ -0,0 +1,32 @@
+﻿using SMS.Contracts;
+using SMS.Data;
+using SMS.Models.HomeModels;
+using SMS.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SMS.Services
+{
+    public class HomeService : IHomeService
+    {
+        private readonly SMSDbContext context;
+
+        public HomeService(SMSDbContext _context)
+        {
+            context = _context;
+        }
+
+        public IEnumerable<AllProductsViewModel> GetProducts()
+        {
+            var products = context.Set<Product>()
+                .Select(p => new AllProductsViewModel()
+                {
+                    ProductId = p.Id,
+                    ProductName = p.Name,
+                    ProductPrice = p.Price.ToString("F2")
+                }).ToList();
+
+            return products;
+        }
+    }
+}

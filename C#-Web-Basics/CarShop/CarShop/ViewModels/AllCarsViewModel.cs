de3f50412695f13e0f0a4230369589a66fe4a0ef
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-17 23:24:21 +0200

    added view models

---
+++ C#-Web-Basics/CarShop/CarShop/ViewModels/AllCarsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CarShop.ViewModels
+{
+    public class AllCarsViewModel
+    {
+        public string Model { get; set; }
+        public string PictureUrl { get; set; }
+
+        public string PlateNumber { get; set; }
+
+        public int Issues { get; set; }
+    }
+}
---
+++ C#-Web-Basics/CarShop/CarShop/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CarShop.Constraints;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarShop.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [StringLength(20, MinimumLength = 4)]
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        [StringLength(20, MinimumLength = 5)]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+
+        public string UserType { get; set; }
+    }
+}

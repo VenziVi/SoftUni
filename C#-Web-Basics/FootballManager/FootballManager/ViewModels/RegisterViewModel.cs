7aff5b9ae7eb7686f2591dd391405da9b0eca0c2
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-20 13:29:52 +0200

    implement view models

---
+++ C#-Web-Basics/FootballManager/FootballManager/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootballManager.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [StringLength(20, MinimumLength = 5)]
+        [Required]
+        public string Username { get; set; }
+
+        [StringLength(60, MinimumLength = 10)]
+        [Required]
+        public string Email { get; set; }
+
+        [StringLength(20, MinimumLength = 5)]
+        [Required]
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}

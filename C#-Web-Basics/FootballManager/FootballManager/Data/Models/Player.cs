977adac6d4b028e826b752fd74bf0cb080b6f4a9
Ventsislav Lazarov <vlazarov86@gmail.com>
2022-02-20 13:28:08 +0200

    implement models

---
+++ C#-Web-Basics/FootballManager/FootballManager/Data/Models/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootballManager.Data.Models
+{
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(80)]
+        public string FullName { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Position { get; set; }
+
+        public byte Speed { get; set; }
+
+        public byte Endurance { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public ICollection<UserPlayer> UserPlayers { get; set; } = new List<UserPlayer>();
+    }
+}
---
+++ C#-Web-Basics/FootballManager/FootballManager/Data/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FootballManager.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(60)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public ICollection<UserPlayer> UserPlayers { get; set; } = new List<UserPlayer>();
+    }
+}
